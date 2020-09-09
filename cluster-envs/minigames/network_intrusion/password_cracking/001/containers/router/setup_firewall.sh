946ab501e31e1389f183d1d10399e329fc38b044
Kim Hammar <kimham@kth.se>
2020-09-09 18:33:32 +0200

    gitignore

--- cluster-envs/minigames/network_intrusion/password_cracking/001/containers/router/setup_firewall.sh
+++ cluster-envs/minigames/network_intrusion/password_cracking/001/containers/router/setup_firewall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $EUID -eq 0 ]]; then
-    route add -net $(SUBNET) netmask $(SUBNET_MASK_2) gw $(IP) || echo "route already added"
+    route add -net $1 netmask $2 gw $3 || echo "route already added"
     exit 0
 else
     echo "cannot setup routing table without sudo"
