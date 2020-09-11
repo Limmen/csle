d747fc0ae5089898d9fc0ab745c6bd06350f550a
Kim Hammar <kimham@kth.se>
2020-09-11 18:51:51 +0200

    add kafka to honeypot machine

--- cluster-envs/minigames/network_intrusion/password_cracking/001/containers/honeypot/docker/start.sh
+++ cluster-envs/minigames/network_intrusion/password_cracking/001/containers/honeypot/docker/start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 nohup /usr/sbin/inspircd --runasroot --debug --nopid & > irc.log
+nohup /usr/local/kafka/bin/zookeeper-server-start.sh /usr/local/kafka/config/zookeeper.properties & > zookeeper.log
+nohup /usr/local/kafka/bin/kafka-server-start.sh /usr/local/kafka/config/server.properties & > kafka.log
 service snmpd restart
 service postfix restart
 service postgresql restart
+service ntp restart
 tail -f /dev/null
