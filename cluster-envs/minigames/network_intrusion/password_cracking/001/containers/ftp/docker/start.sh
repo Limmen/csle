54c7d6617fd75e39af4ebd1ef4837a3ee6e3c75b
Kim Hammar <kimham@kth.se>
2020-09-12 11:22:39 +0200

    add tomcat and ts3 server to ftp machine

--- cluster-envs/minigames/network_intrusion/password_cracking/001/containers/ftp/docker/start.sh
+++ cluster-envs/minigames/network_intrusion/password_cracking/001/containers/ftp/docker/start.sh
@@ -3,4 +3,6 @@
 nohup /usr/sbin/vsftpd &
 nohup /usr/bin/mongod &
 /etc/init.d/inetutils-inetd restart
+/usr/local/tomcat/bin/catalina.sh start
+nohup /teamspeak3-server_linux_amd64/ts3server_startscript.sh start &
 tail -f /dev/null
