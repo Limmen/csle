700a2a56b63f5fb462600fa3e66520c4674f97fe
Kim Hammar <kimham@kth.se>
2020-11-22 20:53:01 +0100

    exp prep

--- python-envs/minigames/network_intrusion/password_cracking/gym-pycr-pwcrack/examples/difficulty_level_2/training/v2/cluster/ppo_baseline/run.py
+++ python-envs/minigames/network_intrusion/password_cracking/gym-pycr-pwcrack/examples/difficulty_level_2/training/v2/cluster/ppo_baseline/run.py
@@ -12,21 +12,21 @@ def default_config() -> ClientConfig:
     """
     :return: Default configuration for the experiment
     """
-    agent_config = AgentConfig(gamma=0.995, alpha=0.0005, epsilon=1, render=False, eval_sleep=0.0,
+    agent_config = AgentConfig(gamma=0.995, alpha=0.0001, epsilon=1, render=False, eval_sleep=0.0,
                                                 min_epsilon=0.01, eval_episodes=0, train_log_frequency=1,
                                                 epsilon_decay=0.9999, video=False, eval_log_frequency=1,
                                                 video_fps=5, video_dir=util.default_output_dir() + "/results/videos",
-                                                num_iterations=400,
+                                                num_iterations=4000,
                                                 eval_render=False, gifs=True,
                                                 gif_dir=util.default_output_dir() + "/results/gifs",
                                                 eval_frequency=100, video_frequency=10,
                                                 save_dir=util.default_output_dir() + "/results/data",
-                                                checkpoint_freq=300, input_dim=11 * 40,
+                                                checkpoint_freq=500, input_dim=11 * 40,
                                                 output_dim=100,
-                                                pi_hidden_dim=128, pi_hidden_layers=1,
-                                                vf_hidden_dim=128, vf_hidden_layers=1,
-                                                shared_hidden_layers=2, shared_hidden_dim=128,
-                                                batch_size=1000,
+                                                pi_hidden_dim=512, pi_hidden_layers=1,
+                                                vf_hidden_dim=512, vf_hidden_layers=1,
+                                                shared_hidden_layers=2, shared_hidden_dim=512,
+                                                batch_size=2000,
                                                 gpu=False, tensorboard=True,
                                                 tensorboard_dir=util.default_output_dir() + "/results/tensorboard",
                                                 optimizer="Adam", lr_exp_decay=False, lr_decay_rate=0.999,
@@ -41,7 +41,7 @@ def default_config() -> ClientConfig:
     env_name = "pycr-pwcrack-medium-cluster-v2"
     cluster_config = ClusterConfig(agent_ip="172.18.2.191", agent_username="agent", agent_pw="agent",
                                    server_connection=False, warmup=True, warmup_iterations=500,
-                                   port_forward_next_port = 2000)
+                                   port_forward_next_port = 3000)
     # cluster_config = ClusterConfig(server_ip="172.31.212.91", agent_ip="172.18.2.191",
     #                                agent_username="agent", agent_pw="agent", server_connection=True,
     #                                server_private_key_file="/home/kim/.ssh/id_rsa",
@@ -54,8 +54,9 @@ def default_config() -> ClientConfig:
                                  agent_type=AgentType.PPO_BASELINE.value,
                                  output_dir=util.default_output_dir(),
                                  title="PPO-Baseline v2",
-                                 run_many=True, random_seeds=[0, 999, 299, 399, 499],
+                                 run_many=True, random_seeds=[0, 999, 299],
                                  random_seed=399, cluster_config=cluster_config, mode=RunnerMode.TRAIN_ATTACKER.value)
+    #random_seeds = [0, 999, 299, 399, 499]
     return client_config
 
 
