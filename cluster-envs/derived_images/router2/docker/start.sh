9af34a0e2780d64f50106d7db6190c0d838aade0
Kim Hammar <kimham@kth.se>
2020-12-06 19:59:09 +0100

    level 4

---
+++ cluster-envs/derived_images/router2/docker/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/usr/sbin/sshd -D &
+
+sudo snort -D -q -u snort -g snort -c /etc/snort/snort.conf -i eth0 -l /var/snort/ -h 172.18.0.0/16
+
+tail -f /dev/null
+
---
+++ python-envs/minigames/network_intrusion/password_cracking/gym-pycr-pwcrack/examples/difficulty_level_4/hello_world/cluster_hello_world.py
@@ -0,0 +1,58 @@
+from gym_pycr_pwcrack.envs.pycr_pwcrack_env import PyCRPwCrackSimpleSim1Env, PyCRPwCrackSimpleCluster1Env
+from gym_pycr_pwcrack.dao.network.cluster_config import ClusterConfig
+import gym
+import time
+import numpy as np
+
+def test_env(env_name : str, num_steps : int):
+    # cluster_config = ClusterConfig(server_ip="172.31.212.91", agent_ip="172.18.4.191",
+    #                                agent_username="agent", agent_pw="agent", server_connection=True,
+    #                                server_private_key_file="/Users/kimham/.ssh/pycr_id_rsa",
+    #                                server_username="kim")
+    cluster_config = ClusterConfig(server_ip="172.31.212.91", agent_ip="172.18.4.191",
+                                   agent_username="agent", agent_pw="agent", server_connection=True,
+                                   server_private_key_file="/home/kim/.ssh/id_rsa",
+                                   server_username="kim")
+    # cluster_config = ClusterConfig(agent_ip="172.18.4.191", agent_username="agent", agent_pw="agent",
+    #                                server_connection=False)
+    env = gym.make(env_name, env_config=None, cluster_config=cluster_config)
+    env.env_config.max_episode_length = 1000000000
+    env.env_config.manual_play = True
+
+
+
+    env.reset()
+
+    num_actions = env.env_config.action_conf.num_actions
+    actions = np.array(list(range(num_actions)))
+    print("num actions:{}".format(num_actions))
+    tot_rew = 0
+    for i in range(num_steps):
+        legal_actions = list(filter(lambda x: env.is_action_legal(x, env.env_config, env.env_state), actions))
+        action = np.random.choice(legal_actions)
+        obs, reward, done, info = env.step(action)
+        tot_rew += reward
+        env.render()
+        if done:
+            print("tot_rew:{}".format(tot_rew))
+            tot_rew = 0
+            env.reset()
+        #time.sleep(0.001)
+        #time.sleep(0.5)
+    env.reset()
+    env.close()
+
+
+def test_all():
+    #test_env("pycr-pwcrack-simple-sim-v1", num_steps=1000000000)
+    test_env("pycr-pwcrack-simple-cluster-v1", num_steps=1000000000)
+    #test_env("pycr-pwcrack-simple-cluster-v2", num_steps=1000000000)
+    #test_env("pycr-pwcrack-simple-cluster-v4", num_steps=1000000000)
+    #test_env("pycr-pwcrack-simple-cluster-nocache-v1", num_steps=1000000000)
+    #test_env("pycr-pwcrack-simple-cluster-base-v1", num_steps=1000000000)
+    #test_env("pycr-pwcrack-simple-cluster-v3", num_steps=1000000000)
+    #test_env("pycr-pwcrack-simple-cluster-v1", num_steps=1000000000)
+    #test_env("pycr-pwcrack-simple-generated-sim-v1", num_steps=1000000000)
+
+if __name__ == '__main__':
+    test_all()
