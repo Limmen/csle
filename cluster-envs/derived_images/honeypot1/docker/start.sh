c9f99ccdb3391005a6feca234d82fd4d18491882
Kim Hammar <kimham@kth.se>
2021-02-28 11:35:41 +0100

    save RAM

--- cluster-envs/derived_images/honeypot1/docker/start.sh
+++ cluster-envs/derived_images/honeypot1/docker/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 nohup /usr/sbin/inspircd --runasroot --debug --nopid & > irc.log
-nohup /usr/local/kafka/bin/zookeeper-server-start.sh /usr/local/kafka/config/zookeeper.properties & > zookeeper.log
-nohup /usr/local/kafka/bin/kafka-server-start.sh /usr/local/kafka/config/server.properties & > kafka.log
+#nohup /usr/local/kafka/bin/zookeeper-server-start.sh /usr/local/kafka/config/zookeeper.properties & > zookeeper.log
+#nohup /usr/local/kafka/bin/kafka-server-start.sh /usr/local/kafka/config/server.properties & > kafka.log
 /usr/sbin/sshd -D &
 service snmpd restart
 service postfix restart
