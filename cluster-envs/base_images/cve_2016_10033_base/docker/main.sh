46d724f098a85e14f80720b61efb99573b61beae
Kim Hammar <kimham@kth.se>
2021-03-09 14:47:47 +0100

    update README

--- cluster-envs/base_images/cve_2016_10033_base/docker/main.sh
+++ cluster-envs/base_images/cve_2016_10033_base/docker/main.sh
@@ -8,4 +8,4 @@ sleep 3
 tail -f /var/log/apache2/*&
 
 # Start our fake smtp server
-python -m smtpd -n -c DebuggingServer localhost:25
+python -m smtpd -n -c DebugingServer localhost:25g
--- cluster-envs/derived_images/sql_injection1/docker/start.sh
+++ cluster-envs/derived_images/sql_injection1/docker/start.sh
@@ -2,7 +2,6 @@
 
 /usr/sbin/sshd -D &
 nohup /usr/sbin/inspircd --runasroot --debug --nopid & > irc.log
-nohup /main.sh &
 sleep 5
 /setup_db.sh
 tail -f /dev/null
