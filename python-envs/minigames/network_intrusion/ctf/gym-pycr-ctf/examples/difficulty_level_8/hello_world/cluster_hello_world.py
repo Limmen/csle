6a3dfd0ee620c4d394d8bc10579677d3a4f9b8f7
Kim Hammar <kimham@kth.se>
2021-03-28 22:27:03 +0200

    test

--- python-envs/minigames/network_intrusion/ctf/gym-pycr-ctf/examples/difficulty_level_8/hello_world/cluster_hello_world.py
+++ python-envs/minigames/network_intrusion/ctf/gym-pycr-ctf/examples/difficulty_level_8/hello_world/cluster_hello_world.py
@@ -7,16 +7,16 @@ import numpy as np
 from gym_pycr_ctf.envs.logic.common.env_dynamics_util import EnvDynamicsUtil
 
 def test_env(env_name : str, num_steps : int):
-    # cluster_config = ClusterConfig(server_ip="172.31.212.91", agent_ip="172.18.8.191",
+    # cluster_config = ClusterConfig(server_ip="172.31.212.92", agent_ip="172.18.8.191",
     #                                agent_username="agent", agent_pw="agent", server_connection=True,
     #                                server_private_key_file="/Users/kimham/.ssh/pycr_id_rsa",
     #                                server_username="kim")
-    # cluster_config = ClusterConfig(server_ip="172.31.212.91", agent_ip="172.18.8.191",
-    #                                agent_username="agent", agent_pw="agent", server_connection=True,
-    #                                server_private_key_file="/home/kim/.ssh/id_rsa",
-    #                                server_username="kim")
-    cluster_config = ClusterConfig(agent_ip="172.18.8.191", agent_username="agent", agent_pw="agent",
-                                   server_connection=False)
+    cluster_config = ClusterConfig(server_ip="172.31.212.92", agent_ip="172.18.8.191",
+                                   agent_username="agent", agent_pw="agent", server_connection=True,
+                                   server_private_key_file="/home/kim/.ssh/id_rsa",
+                                   server_username="kim")
+    # cluster_config = ClusterConfig(agent_ip="172.18.8.191", agent_username="agent", agent_pw="agent",
+    #                                server_connection=False)
     env = gym.make(env_name, env_config=None, cluster_config=cluster_config)
     env.env_config.max_episode_length = 1000000000
     env.env_config.manual_play = False
@@ -29,16 +29,23 @@ def test_env(env_name : str, num_steps : int):
     tot_rew = 0
     tried_actions = []
     for i in range(num_steps):
-        print("i:{}".format(i))
+        #print("i:{}".format(i))
         legal_actions = list(filter(lambda x: env.is_action_legal(x, env.env_config, env.env_state), actions))
         if len(legal_actions) == 0:
             print("no legal actions, {}".format(tried_actions))
             print(env.env_state.obs_state.actions_tried)
             for m in env.env_state.obs_state.machines:
                 print(
-                    "ip: {}, shell access:{}, ssh_brute_t:{}, ftp_brute_t:{}, telnet_brute_t:{}, fs_searched:{},untried_cred:{},logged_in:{},"
-                    "tools:{},backdoor:{}, flags found:{}".format(
+                    "ip: {}, shell access:{}, ssh_brute_t:{}, ftp_brute_t:{}, telnet_brute_t:{}, "
+                    "samba_tried:{},shellsock_tried:{},dvwa_sql_injection_tried:{},"
+                    "cve_2015_3306_tried:{}, cve_2015_1427_tried:{}, cve_2016_10033_tried:{},"
+                    "cve_2010_0426_tried:{},cve_2015_5602_tried:{}"
+                    "fs_searched:{},untried_cred:{},logged_in:{},"
+                    "tools:{},backdoor:{},flags found:{}".format(
                         m.ip, m.shell_access, m.telnet_brute_tried, m.ssh_brute_tried, m.ftp_brute_tried,
+                        m.sambacry_tried, m.shellshock_tried, m.dvwa_sql_injection_tried,
+                        m.cve_2015_3306_tried, m.cve_2015_1427_tried,
+                        m.cve_2016_10033_tried, m.cve_2010_0426_tried, m.cve_2015_5602_tried,
                         m.filesystem_searched, m.untried_credentials,
                         m.logged_in, m.tools_installed, m.backdoor_installed, m.flags_found))
             print("all flags?:{}".format(
