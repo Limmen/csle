018d9b58cf0c9dda6bd4617f1111289d7fabaf71
Kim Hammar <kimham@kth.se>
2021-04-13 23:13:11 +0200

    update dependencies

--- python-envs/minigames/network_intrusion/ctf/gym-pycr-ctf/setup.py
+++ python-envs/minigames/network_intrusion/ctf/gym-pycr-ctf/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='gym_pycr_ctf',
       version='0.0.1',
       install_requires=['gym', 'pyglet', 'numpy', 'torch', 'docker', 'paramiko', 'stable_baselines3', 'scp',
-                        'random_username'],
+                        'random_username', 'jsonpickle'],
       author='Kim Hammar',
       author_email='hammar.kim@gmail.com',
       description='Python Agent Cyber Range CTF MiniGame',
