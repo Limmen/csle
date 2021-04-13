b0b7013da9e5003c5d25539b7ba9a65fb1d26e7a
Kim Hammar <kimham@kth.se>
2021-04-13 22:12:52 +0200

    upload cache script

--- python-envs/minigames/network_intrusion/ctf/gym-pycr-ctf/examples/difficulty_level_9/training/v1/generated_simulation/attacker/ppo_baseline/run.py
+++ python-envs/minigames/network_intrusion/ctf/gym-pycr-ctf/examples/difficulty_level_9/training/v1/generated_simulation/attacker/ppo_baseline/run.py
@@ -14,11 +14,11 @@ def default_config() -> ClientConfig:
     """
     :return: Default configuration for the experiment
     """
-    agent_config = AgentConfig(gamma=1, alpha=0.0001, epsilon=1, render=False, eval_sleep=0.0,
+    agent_config = AgentConfig(gamma=1, alpha=0.00005, epsilon=1, render=False, eval_sleep=0.0,
                                min_epsilon=0.01, eval_episodes=0, train_log_frequency=1,
                                epsilon_decay=0.9999, video=False, eval_log_frequency=1,
                                video_fps=5, video_dir=util.default_output_dir() + "/results/videos",
-                               num_iterations=200,
+                               num_iterations=400,
                                eval_render=False, gifs=False,
                                gif_dir=util.default_output_dir() + "/results/gifs",
                                eval_frequency=500000, video_frequency=10,
@@ -39,7 +39,7 @@ def default_config() -> ClientConfig:
                                render_steps=100, illegal_action_logit=-1000,
                                filter_illegal_actions=True, train_progress_deterministic_eval=True,
                                attacker_opponent_baseline_type=8,
-                               running_avg=50, n_quick_eval_iter=5, n_deterministic_eval_iter=5,
+                               running_avg=50, n_quick_eval_iter=5, n_deterministic_eval_iter=0,
                                log_regret=True, snort_baseline_simulate=False, quick_eval_freq=1,
                                eval_deterministic=False
                                )
--- python-envs/minigames/network_intrusion/ctf/gym-pycr-ctf/gym_pycr_ctf/envs_model/config/generator/generator_util.py
+++ python-envs/minigames/network_intrusion/ctf/gym-pycr-ctf/gym_pycr_ctf/envs_model/config/generator/generator_util.py
@@ -1,3 +1,4 @@
+import os
 from scp import SCPClient
 from gym_pycr_ctf.dao.network.emulation_config import EmulationConfig
 from gym_pycr_ctf.dao.container_config.containers_config import ContainersConfig
@@ -32,11 +33,34 @@ class GeneratorUtil:
     @staticmethod
     def zip_and_download_filesystem_cache(containers_config: ContainersConfig):
         for c in containers_config.containers:
+            print("Downloading cache from container:{}".format(c.name))
             emulation_config = EmulationConfig(agent_ip=c.ip, agent_username="pycr_admin",
                                              agent_pw="pycr@admin-pw_191", server_connection=False)
             GeneratorUtil.connect_admin(emulation_config=emulation_config, ip=c.ip)
             filename = c.name + "_cache.zip"
-            EmulationUtil.execute_ssh_cmd(cmd="zip -r " + filename + " /home/ '*.xml' '*.txt'", conn=emulation_config.agent_conn)
+            o,e, _ = EmulationUtil.execute_ssh_cmd(cmd="zip -r " + filename + " /home/ '*.xml' '*.txt'", conn=emulation_config.agent_conn)
             filepath = "/home/pycr_admin/"+ filename
             scp_client = SCPClient(emulation_config.agent_conn.get_transport())
             scp_client.get(filepath)
+
+    @staticmethod
+    def upload_and_unzip_filesystem_cache(containers_config: ContainersConfig):
+        for c in containers_config.containers:
+            print("Uploading cache for container:{}".format(c.name))
+            local_filename = c.name + "_cache.zip"
+            if os.path.exists(local_filename):
+                emulation_config = EmulationConfig(agent_ip=c.ip, agent_username="pycr_admin",
+                                                   agent_pw="pycr@admin-pw_191", server_connection=False)
+                GeneratorUtil.connect_admin(emulation_config=emulation_config, ip=c.ip)
+                scp_client = SCPClient(emulation_config.agent_conn.get_transport())
+                remote_filename = "/home/pycr_admin/" + local_filename
+                scp_client.put(local_filename, remote_filename)
+                unzip_cmd = "cd /home/pycr_admin/; unzip " + remote_filename
+                o, e, _ = EmulationUtil.execute_ssh_cmd(cmd=unzip_cmd, conn=emulation_config.agent_conn)
+                cmd = "ls /home/pycr_admin/home/"
+                o, e, _ = EmulationUtil.execute_ssh_cmd(cmd=cmd, conn=emulation_config.agent_conn)
+                users_w_home = o.decode().split("\n")
+                users_w_home = list(filter(lambda x: x != '', users_w_home))
+                for user in users_w_home:
+                    cmd = "cp -f /home/pycr_admin/home/" + user + "/* /home/" + user + "/"
+                    o, e, _ = EmulationUtil.execute_ssh_cmd(cmd=cmd, conn=emulation_config.agent_conn)
