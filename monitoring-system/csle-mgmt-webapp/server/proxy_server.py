d726cefff247a911a9efc19d24dc0af0772c972e
Kim Hammar <kimham@kth.se>
2022-07-09 13:59:40 +0200

    bugfix

---
+++ monitoring-system/csle-mgmt-webapp/server/proxy_server.py
@@ -0,0 +1,7 @@
+import csle_rest_api.rest_api_proxy as rest_api_proxy
+
+if __name__ == '__main__':
+    static_folder_path = "../../../../monitoring-system/csle-mgmt-webapp/build"
+    proxy_server = 'http://172.31.212.92:7777/'
+    rest_api_proxy.start_proxy_server(static_folder=static_folder_path, port=7777, num_threads=100, host="0.0.0.0",
+                                      proxy_server=proxy_server)
--- simulation-system/python/csle-rest-api/csle_rest_api/rest_api_proxy.py
+++ simulation-system/python/csle-rest-api/csle_rest_api/rest_api_proxy.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from waitress import serve
 from requests import get
 import csle_common.constants.constants as constants
 import csle_rest_api.constants.constants as api_constants
@@ -16,81 +17,74 @@ from csle_rest_api.pages.policy_examination.routes import policy_examination_pag
 from csle_rest_api.pages.training.routes import training_page_bp
 from csle_rest_api.pages.sdn_controllers.routes import sdn_controllers_page_bp
 
-# app = Flask(__name__)
-SITE_NAME = 'http://172.31.212.92:7777/'
-static_folder = "../../../../monitoring-system/csle-mgmt-webapp/build"
 
-app = Flask(__name__, static_url_path='', static_folder=static_folder)
+def create_app(static_folder: str, proxy_server: str):
+    """
+    Creates and initializes the Flask App
 
-app.register_blueprint(emulations_page_bp,
-                       url_prefix=f"{constants.COMMANDS.SLASH_DELIM}{api_constants.MGMT_WEBAPP.EMULATIONS_PAGE_RESOURCE}")
-app.register_blueprint(simulations_page_bp,
-                       url_prefix=f"{constants.COMMANDS.SLASH_DELIM}{api_constants.MGMT_WEBAPP.SIMULATIONS_PAGE_RESOURCE}")
-app.register_blueprint(traces_page_bp,
-                       url_prefix=f"{constants.COMMANDS.SLASH_DELIM}{api_constants.MGMT_WEBAPP.TRACES_PAGE_RESOURCE}")
-app.register_blueprint(monitoring_page_bp,
-                       url_prefix=f"{constants.COMMANDS.SLASH_DELIM}{api_constants.MGMT_WEBAPP.MONITORING_PAGE_RESOURCE}")
-app.register_blueprint(emulation_statistics_page_bp,
-                       url_prefix=f"{constants.COMMANDS.SLASH_DELIM}"
-                                  f"{api_constants.MGMT_WEBAPP.EMULATION_STATISTICS_PAGE_RESOURCE}")
-app.register_blueprint(system_models_page_bp,
-                       url_prefix=f"{constants.COMMANDS.SLASH_DELIM}"
-                                  f"{api_constants.MGMT_WEBAPP.SYSTEM_MODELS_PAGE_RESOURCE}")
-app.register_blueprint(about_page_bp,
-                       url_prefix=f"{constants.COMMANDS.SLASH_DELIM}"
-                                  f"{api_constants.MGMT_WEBAPP.ABOUT_PAGE_RESOURCE}")
-app.register_blueprint(images_page_bp,
-                       url_prefix=f"{constants.COMMANDS.SLASH_DELIM}{api_constants.MGMT_WEBAPP.IMAGES_PAGE_RESOURCE}")
-app.register_blueprint(jobs_page_bp,
-                       url_prefix=f"{constants.COMMANDS.SLASH_DELIM}{api_constants.MGMT_WEBAPP.JOBS_PAGE_RESOURCE}")
-app.register_blueprint(policies_page_bp,
-                       url_prefix=f"{constants.COMMANDS.SLASH_DELIM}"
-                                  f"{api_constants.MGMT_WEBAPP.POLICIES_PAGE_RESOURCE}")
-app.register_blueprint(policy_examination_page_bp,
-                       url_prefix=f"{constants.COMMANDS.SLASH_DELIM}"
-                                  f"{api_constants.MGMT_WEBAPP.POLICY_EXAMINATION_PAGE_RESOURCE}")
-app.register_blueprint(training_page_bp,
-                       url_prefix=f"{constants.COMMANDS.SLASH_DELIM}"
-                                  f"{api_constants.MGMT_WEBAPP.TRAINING_PAGE_RESOURCE}")
-app.register_blueprint(sdn_controllers_page_bp,
-                       url_prefix=f"{constants.COMMANDS.SLASH_DELIM}"
-                                  f"{api_constants.MGMT_WEBAPP.SDN_CONTROLLERS_PAGE_RESOURCE}")
+    :param static_folder: path to the folder to serve static resources
+    :param proxy_server: proxy server
+    :return: the flask app
+    """
+    app = Flask(__name__, static_url_path='', static_folder=static_folder)
 
-@app.route('/emulations')
-def proxy():
-    return get(f'{SITE_NAME}emulations').content
+    app.register_blueprint(emulations_page_bp,
+                           url_prefix=f"{constants.COMMANDS.SLASH_DELIM}{api_constants.MGMT_WEBAPP.EMULATIONS_PAGE_RESOURCE}")
+    app.register_blueprint(simulations_page_bp,
+                           url_prefix=f"{constants.COMMANDS.SLASH_DELIM}{api_constants.MGMT_WEBAPP.SIMULATIONS_PAGE_RESOURCE}")
+    app.register_blueprint(traces_page_bp,
+                           url_prefix=f"{constants.COMMANDS.SLASH_DELIM}{api_constants.MGMT_WEBAPP.TRACES_PAGE_RESOURCE}")
+    app.register_blueprint(monitoring_page_bp,
+                           url_prefix=f"{constants.COMMANDS.SLASH_DELIM}{api_constants.MGMT_WEBAPP.MONITORING_PAGE_RESOURCE}")
+    app.register_blueprint(emulation_statistics_page_bp,
+                           url_prefix=f"{constants.COMMANDS.SLASH_DELIM}"
+                                      f"{api_constants.MGMT_WEBAPP.EMULATION_STATISTICS_PAGE_RESOURCE}")
+    app.register_blueprint(system_models_page_bp,
+                           url_prefix=f"{constants.COMMANDS.SLASH_DELIM}"
+                                      f"{api_constants.MGMT_WEBAPP.SYSTEM_MODELS_PAGE_RESOURCE}")
+    app.register_blueprint(about_page_bp,
+                           url_prefix=f"{constants.COMMANDS.SLASH_DELIM}"
+                                      f"{api_constants.MGMT_WEBAPP.ABOUT_PAGE_RESOURCE}")
+    app.register_blueprint(images_page_bp,
+                           url_prefix=f"{constants.COMMANDS.SLASH_DELIM}{api_constants.MGMT_WEBAPP.IMAGES_PAGE_RESOURCE}")
+    app.register_blueprint(jobs_page_bp,
+                           url_prefix=f"{constants.COMMANDS.SLASH_DELIM}{api_constants.MGMT_WEBAPP.JOBS_PAGE_RESOURCE}")
+    app.register_blueprint(policies_page_bp,
+                           url_prefix=f"{constants.COMMANDS.SLASH_DELIM}"
+                                      f"{api_constants.MGMT_WEBAPP.POLICIES_PAGE_RESOURCE}")
+    app.register_blueprint(policy_examination_page_bp,
+                           url_prefix=f"{constants.COMMANDS.SLASH_DELIM}"
+                                      f"{api_constants.MGMT_WEBAPP.POLICY_EXAMINATION_PAGE_RESOURCE}")
+    app.register_blueprint(training_page_bp,
+                           url_prefix=f"{constants.COMMANDS.SLASH_DELIM}"
+                                      f"{api_constants.MGMT_WEBAPP.TRAINING_PAGE_RESOURCE}")
+    app.register_blueprint(sdn_controllers_page_bp,
+                           url_prefix=f"{constants.COMMANDS.SLASH_DELIM}"
+                                      f"{api_constants.MGMT_WEBAPP.SDN_CONTROLLERS_PAGE_RESOURCE}")
 
-@app.route('/<path:path>/<path:path2>')
-def proxy_1(path, path2):
-    return get(f'{SITE_NAME}{path}/{path2}').content
+    @app.route(constants.COMMANDS.SLASH_DELIM, methods=[api_constants.MGMT_WEBAPP.HTTP_REST_GET])
+    def root():
+        return app.send_static_file(api_constants.MGMT_WEBAPP.STATIC_RESOURCE_INDEX)
 
-@app.route('/<path:path>/<path:path2>/<path:path3>')
-def proxy_2(path, path2, path3):
-    return get(f'{SITE_NAME}{path}/{path2}/{path3}').content
+    @app.route(f'/{api_constants.MGMT_WEBAPP.EMULATIONS_RESOURCE}')
+    def emulations_proxy():
+        return get(f'{proxy_server}{api_constants.MGMT_WEBAPP.EMULATIONS_RESOURCE}').content
 
-@app.route('/<path:path>/<path:path2>/<path:path3>/<path:path4>')
-def proxy_3(path, path2, path3, path4):
-    return get(f'{SITE_NAME}{path}/{path2}/{path3}/{path4}').content
+    return app
 
-@app.route('/<path:path>/<path:path2>/<path:path3>/<path:path4>/<path:path5>')
-def proxy_4(path, path2, path3, path4, path5):
-    return get(f'{SITE_NAME}{path}/{path2}/{path3}/{path4}/{path5}').content
 
-# @app.route('/static/<path:path1>')
-# def static_1(path1):
-#     return get(f'{SITE_NAME}static/{path1}').content
-#
-# @app.route('/static/<path:path1>/<path:path2>')
-# def static_2(path1, path2):
-#     return get(f'{SITE_NAME}static/{path1}/{path2}').content
-#
-# @app.route('/static/<path:path1>/<path:path2>/<path:path3>')
-# def static_3(path1, path2, path3):
-#     return get(f'{SITE_NAME}static/{path1}/{path2}/{path3}').content
-#
-# @app.route('/static/<path:path1>/<path:path2>/<path:path3>/<path:path4>')
-# def static_4(path1, path2, path3, path4):
-#     return get(f'{SITE_NAME}static/{path1}/{path2}/{path3}/{path4}').content
+def start_proxy_server(static_folder: str, port: int = 7777, proxy_server: str = "http://172.31.212.92:7777/",
+                       num_threads: int = 100, host: str = "0.0.0.0") -> None:
+    """
+    Creates the flasp app and serves it
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7777)
+    :param static_folder: path to the folder to server static resources
+    :param port: the port for serving
+    :param num_threads: number of threads for serving
+    :param host: the host string for serving
+    :param proxy_server: the proxy server
+
+    :return: None
+    """
+    app = create_app(static_folder=static_folder, proxy_server=proxy_server)
+    serve(app, host=host, port=port, threads=num_threads)
