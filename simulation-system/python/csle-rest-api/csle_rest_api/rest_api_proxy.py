eaaeb66512ca10083c929f0edab88da05a46ca1d
Kim Hammar <kimham@kth.se>
2022-07-09 13:13:57 +0200

    bugfix

--- simulation-system/python/csle-rest-api/csle_rest_api/rest_api_proxy.py
+++ simulation-system/python/csle-rest-api/csle_rest_api/rest_api_proxy.py
@@ -7,7 +7,28 @@ SITE_NAME = 'http://172.31.212.92:7777/'
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def proxy(path):
+    print("path1")
     return get(f'{SITE_NAME}{path}').content
+
+@app.route('/<path:path>/<path2:path2>')
+def proxy_1(path, path2):
+    print("path2")
+    return get(f'{SITE_NAME}{path}/{path2}').content
+
+@app.route('/<path:path>/<path2:path2>/<path3:path3>')
+def proxy_2(path, path2, path3):
+    print("path3")
+    return get(f'{SITE_NAME}{path}/{path2}/{path3}').content
+
+@app.route('/<path:path>/<path2:path2>/<path3:path3>/<path4:path4>')
+def proxy_3(path, path2, path3, path4):
+    print("path4")
+    return get(f'{SITE_NAME}{path}/{path2}/{path3}/{path4}').content
+
+@app.route('/<path:path>/<path2:path2>/<path3:path3>/<path4:path4>/<path5:path5>')
+def proxy_4(path, path2, path3, path4, path5):
+    print("path5")
+    return get(f'{SITE_NAME}{path}/{path2}/{path3}/{path4}/{path5}').content
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7777)
