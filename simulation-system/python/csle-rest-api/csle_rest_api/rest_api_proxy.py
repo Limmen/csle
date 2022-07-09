64bfdd71f5f3587f7ba03ac8a0aab96753ca3c55
Kim Hammar <kimham@kth.se>
2022-07-09 13:23:29 +0200

    bugfix

--- simulation-system/python/csle-rest-api/csle_rest_api/rest_api_proxy.py
+++ simulation-system/python/csle-rest-api/csle_rest_api/rest_api_proxy.py
@@ -30,5 +30,25 @@ def proxy_4(path, path2, path3, path4, path5):
     print("path5")
     return get(f'{SITE_NAME}{path}/{path2}/{path3}/{path4}/{path5}').content
 
+@app.route('/static/<path:path1>')
+def static_1(path1):
+    print("static")
+    return get(f'{SITE_NAME}static/{path1}').content
+
+@app.route('/static/<path:path1>/<path:path2>')
+def static_2(path1, path2):
+    print("static2")
+    return get(f'{SITE_NAME}static/{path1}/{path2}').content
+
+@app.route('/static/<path:path1>/<path:path2>/<path:path3>')
+def static_3(path1, path2, path3):
+    print("static3")
+    return get(f'{SITE_NAME}static/{path1}/{path2}/{path3}').content
+
+@app.route('/static/<path:path1>/<path:path2>/<path:path3>/<path:path4>')
+def static_4(path1, path2, path3, path4):
+    print("static4")
+    return get(f'{SITE_NAME}static/{path1}/{path2}/{path3}/{path4}').content
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7777)
