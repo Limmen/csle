f2f8a8c8d714b7e06955a4715a377a009795d241
Limmen <kimham@kth.se>
2022-07-28 13:27:18 +0200

    bugfix

---
+++ simulation-system/python/csle-rest-api/csle_rest_api/pages/downloads/routes.py
@@ -0,0 +1,22 @@
+"""
+Routes and resources for the /downloads-page page
+"""
+from flask import Blueprint
+import csle_common.constants.constants as constants
+import csle_rest_api.constants.constants as api_constants
+
+# Creates a blueprint "sub application" of the main REST app
+downloads_page_bp = Blueprint(
+    api_constants.MGMT_WEBAPP.DOWNLOADS_PAGE_RESOURCE, __name__,
+    url_prefix=f"{constants.COMMANDS.SLASH_DELIM}{api_constants.MGMT_WEBAPP.DOWNLOADS_PAGE_RESOURCE}",
+    static_url_path=f'{constants.COMMANDS.SLASH_DELIM}{api_constants.MGMT_WEBAPP.DOWNLOADS_PAGE_RESOURCE}'
+                    f'{constants.COMMANDS.SLASH_DELIM}{api_constants.MGMT_WEBAPP.STATIC}',
+    static_folder="../../../../../../monitoring-system/csle-mgmt-webapp/build")
+
+
+@downloads_page_bp.route("", methods=[api_constants.MGMT_WEBAPP.HTTP_REST_GET])
+def downloads_page():
+    """
+    :return: static resources for the /downloads-page url
+    """
+    return downloads_page_bp.send_static_file(api_constants.MGMT_WEBAPP.STATIC_RESOURCE_INDEX)
