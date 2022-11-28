df95ee4d3022b6a44c5b2ac6d5035c26182d4170
Limmen <kimham@kth.se>
2022-11-28 15:59:28 +0100

    Update README

--- management-system/csle-mgmt-webapp/server/server.py
+++ management-system/csle-mgmt-webapp/server/server.py
@@ -1,6 +1,6 @@
 import csle_rest_api.rest_api as rest_api
 
 if __name__ == '__main__':
-    static_folder_path = "../../../../management-system/csle-mgmt-webapp/build"
-    app_path = "../../../../management-system/csle-mgmt-webapp/server/server.py"
+    static_folder_path = "../../../../../management-system/csle-mgmt-webapp/build"
+    app_path = "../../../../../management-system/csle-mgmt-webapp/server/server.py"
     rest_api.start_server(static_folder=static_folder_path, port=7777, num_threads=100, host="0.0.0.0", https=False)
--- simulation-system/libs/csle-rest-api/src/csle_rest_api/rest_api.py
+++ simulation-system/libs/csle-rest-api/src/csle_rest_api/rest_api.py
@@ -279,5 +279,6 @@ def start_server(static_folder: str, port: int = 7777, num_threads: int = 100, h
     # run(f"gunicorn -b {host}:{port} 'csle_rest_api.rest_api:create_app() --workers 4 --threads
     # {num_threads}'".split(' '))
     # gunicorn -b 0.0.0.0:5000 --workers 4 --threads num_threads module:app
+    print(f"Starting web server, listening on port: {port}")
     app = create_app(static_folder=static_folder)
     socketio.run(app, debug=False, port=port, host=host)
