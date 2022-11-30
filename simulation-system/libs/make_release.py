97a16923ce763c05ca76fee4997bd2662d3e6a07
Kim Hammar <kimham@kth.se>
2022-11-30 08:29:53 +0100

    Filebeats [WIP]

--- simulation-system/libs/make_release.py
+++ simulation-system/libs/make_release.py
@@ -8,7 +8,7 @@ RELEASE_CONFIG = {
         "new_version": "'0.0.25'",
     },
     "csle-collector": {
-        "new_version": "'0.0.77'",
+        "new_version": "'0.0.78'",
     },
     "csle-common": {
         "new_version": "'0.0.7'",
