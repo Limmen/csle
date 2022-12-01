8e93a9d92bb1a0f77cdcc03844d230932707f039
Limmen <kimham@kth.se>
2022-12-01 10:12:40 +0100

    beats [WIP]

--- simulation-system/libs/csle-common/src/csle_common/controllers/host_controller.py
+++ simulation-system/libs/csle-common/src/csle_common/controllers/host_controller.py
@@ -26,9 +26,19 @@ class HostController:
         :param emulation_env_config: the emulation env config
         :return: None
         """
+
+        # Start host managers on emulation containers
         for c in emulation_env_config.containers_config.containers:
             # Connect
             HostController.start_host_manager(emulation_env_config=emulation_env_config, ip=c.get_ips()[0])
+
+        # Start host manager on kafka container
+        HostController.start_host_manager(emulation_env_config=emulation_env_config,
+                                          ip=emulation_env_config.kafka_config.container.get_ips()[0])
+
+        # Start host manager on ELK container
+        HostController.start_host_manager(emulation_env_config=emulation_env_config,
+                                          ip=emulation_env_config.elk_config.container.get_ips()[0])
 
     @staticmethod
     def start_host_manager(emulation_env_config: EmulationEnvConfig, ip: str) -> None:
@@ -75,8 +85,17 @@ class HostController:
         :param emulation_env_config: the emulation env config
         :return: None
         """
+        # Stop host manager on emulation containers
         for c in emulation_env_config.containers_config.containers:
             HostController.stop_host_manager(emulation_env_config=emulation_env_config, ip=c.get_ips()[0])
+
+        # Stop host manager on Kafka container
+        HostController.stop_host_manager(emulation_env_config=emulation_env_config,
+                                         ip=emulation_env_config.kafka_config.container.get_ips()[0])
+
+        # Stop host manager on ELK container
+        HostController.stop_host_manager(emulation_env_config=emulation_env_config,
+                                         ip=emulation_env_config.elk_config.container.get_ips()[0])
 
     @staticmethod
     def stop_host_manager(emulation_env_config: EmulationEnvConfig, ip: str) -> None:
@@ -107,8 +126,16 @@ class HostController:
         :param emulation_env_config: the emulation env config
         :return: None
         """
+        # Start host monitor on emulation containers
         for c in emulation_env_config.containers_config.containers:
             HostController.start_host_monitor_thread(emulation_env_config=emulation_env_config, ip=c.get_ips()[0])
+
+        # Start host monitor on the Kafka container
+        HostController.start_host_monitor_thread(emulation_env_config=emulation_env_config,
+                                                 ip=emulation_env_config.kafka_config.container.get_ips()[0])
+        # Start host monitor on the ELK container
+        HostController.start_host_monitor_thread(emulation_env_config=emulation_env_config,
+                                                 ip=emulation_env_config.elk_config.container.get_ips()[0])
 
     @staticmethod
     def start_filebeats(emulation_env_config: EmulationEnvConfig) -> None:
@@ -119,8 +146,16 @@ class HostController:
         :param emulation_env_config: the emulation env config
         :return: None
         """
+        # Start filebeat on emulation containers
         for c in emulation_env_config.containers_config.containers:
             HostController.start_filebeat(emulation_env_config=emulation_env_config, ip=c.get_ips()[0])
+
+        # Start host monitor on the Kafka container
+        HostController.start_filebeat(emulation_env_config=emulation_env_config,
+                                                 ip=emulation_env_config.kafka_config.container.get_ips()[0])
+        # Start host monitor on the ELK container
+        HostController.start_filebeat(emulation_env_config=emulation_env_config,
+                                                 ip=emulation_env_config.elk_config.container.get_ips()[0])
 
     @staticmethod
     def stop_filebeats(emulation_env_config: EmulationEnvConfig) -> None:
@@ -131,8 +166,17 @@ class HostController:
         :param emulation_env_config: the emulation env config
         :return: None
         """
+        # Stop filebeat on emulation containers
         for c in emulation_env_config.containers_config.containers:
             HostController.stop_filebeat(emulation_env_config=emulation_env_config, ip=c.get_ips()[0])
+
+        # Stop filebeat on the kafka container
+        HostController.stop_filebeat(emulation_env_config=emulation_env_config,
+                                     ip=emulation_env_config.kafka_config.container.get_ips()[0])
+
+        # Stop filebeat on the ELK container
+        HostController.stop_filebeat(emulation_env_config=emulation_env_config,
+                                     ip=emulation_env_config.elk_config.container.get_ips()[0])
 
     @staticmethod
     def config_filebeats(emulation_env_config: EmulationEnvConfig) -> None:
@@ -143,8 +187,17 @@ class HostController:
         :param emulation_env_config: the emulation env config
         :return: None
         """
+        # Configure filebeat on the emulation containers
         for c in emulation_env_config.containers_config.containers:
             HostController.config_filebeat(emulation_env_config=emulation_env_config, container=c)
+
+        # Configure filebeat on the kafka container
+        HostController.config_filebeat(emulation_env_config=emulation_env_config,
+                                       container=emulation_env_config.kafka_config.container)
+
+        # Configure filebeat on the ELK container
+        HostController.config_filebeat(emulation_env_config=emulation_env_config,
+                                       container=emulation_env_config.elk_config.container)
 
     @staticmethod
     def start_host_monitor_thread(emulation_env_config: EmulationEnvConfig, ip: str) -> None:
@@ -236,8 +289,17 @@ class HostController:
         :param emulation_env_config: the emulation env config
         :return: None
         """
+        # Stop host monitor threads on emulation containers
         for c in emulation_env_config.containers_config.containers:
             HostController.stop_host_monitor_thread(emulation_env_config=emulation_env_config, ip=c.get_ips()[0])
+
+        # Stop host monitor threads on the kafka container
+        HostController.stop_host_monitor_thread(emulation_env_config=emulation_env_config,
+                                                ip=emulation_env_config.kafka_config.container.get_ips()[0])
+
+        # Stop host monitor threads on the ELK container
+        HostController.stop_host_monitor_thread(emulation_env_config=emulation_env_config,
+                                                ip=emulation_env_config.elk_config.container.get_ips()[0])
 
     @staticmethod
     def stop_host_monitor_thread(emulation_env_config: EmulationEnvConfig, ip: str) -> None:
@@ -288,10 +350,25 @@ class HostController:
         """
         statuses = []
         HostController.start_host_managers(emulation_env_config=emulation_env_config)
+
+        # Get statuses of emulation containers
         for c in emulation_env_config.containers_config.containers:
             status = HostController.get_host_monitor_thread_status_by_port_and_ip(
                 ip=c.get_ips()[0], port=emulation_env_config.host_manager_config.host_manager_port)
             statuses.append((status, c.get_ips()[0]))
+
+        # Get status of kafka container
+        status = HostController.get_host_monitor_thread_status_by_port_and_ip(
+            ip=emulation_env_config.kafka_config.container.get_ips()[0],
+            port=emulation_env_config.host_manager_config.host_manager_port)
+        statuses.append((status, emulation_env_config.kafka_config.container.get_ips()[0]))
+
+        # Get status of ELK container
+        status = HostController.get_host_monitor_thread_status_by_port_and_ip(
+            ip=emulation_env_config.elk_config.container.get_ips()[0],
+            port=emulation_env_config.host_manager_config.host_manager_port)
+        statuses.append((status, emulation_env_config.elk_config.container.get_ips()[0]))
+
         return statuses
 
     @staticmethod
@@ -301,12 +378,12 @@ class HostController:
         A method that sends a request to the HostManager on a specific container
         to get the status of the Host monitor thread
 
-        :param emulation_env_config: the emulation config
+        :param ip: the ip of the container
+        :param port: the port of the host manager
         :return: the status of the host manager
         """
         # Open a gRPC session
-        with grpc.insecure_channel(
-                f'{ip}:{port}') as channel:
+        with grpc.insecure_channel(f'{ip}:{port}') as channel:
             stub = csle_collector.host_manager.host_manager_pb2_grpc.HostManagerStub(channel)
             status = csle_collector.host_manager.query_host_manager.get_host_status(stub=stub)
             return status
@@ -327,16 +404,49 @@ class HostController:
         host_metrics_data_list = []
         HostController.start_host_managers(emulation_env_config=emulation_env_config)
 
+        # Get log data of emulation containers
         for c in emulation_env_config.containers_config.containers:
-            # Open a gRPC session
-            with grpc.insecure_channel(
-                    f'{c.get_ips()[0]}:'
-                    f'{emulation_env_config.host_manager_config.host_manager_port}') as channel:
-                stub = csle_collector.host_manager.host_manager_pb2_grpc.HostManagerStub(channel)
-                host_metrics_data = csle_collector.host_manager.query_host_manager.get_host_metrics(
-                    stub=stub, failed_auth_last_ts=failed_auth_last_ts, login_last_ts=login_last_ts)
-                host_metrics_data_list.append(host_metrics_data)
+            host_metrics_data = HostController.get_host_log_data_by_port_and_ip(
+                ip=c.get_ips()[0], port=emulation_env_config.host_manager_config.host_manager_port,
+            failed_auth_last_ts=failed_auth_last_ts, login_last_ts=login_last_ts)
+            host_metrics_data_list.append(host_metrics_data)
+
+        # Get log data from kafka container
+        host_metrics_data = HostController.get_host_log_data_by_port_and_ip(
+            ip=emulation_env_config.kafka_config.container.get_ips()[0],
+            port=emulation_env_config.host_manager_config.host_manager_port,
+            failed_auth_last_ts=failed_auth_last_ts, login_last_ts=login_last_ts)
+        host_metrics_data_list.append(host_metrics_data)
+
+        # Get log data from ELK container
+        host_metrics_data = HostController.get_host_log_data_by_port_and_ip(
+            ip=emulation_env_config.elk_config.container.get_ips()[0],
+            port=emulation_env_config.host_manager_config.host_manager_port,
+            failed_auth_last_ts=failed_auth_last_ts, login_last_ts=login_last_ts)
+        host_metrics_data_list.append(host_metrics_data)
+
         return host_metrics_data_list
+
+    @staticmethod
+    def get_host_log_data_by_port_and_ip(ip: str, port: int, failed_auth_last_ts: float,
+                                         login_last_ts: float) -> \
+            csle_collector.host_manager.host_manager_pb2.HostMetricsDTO:
+        """
+        A method that sends a request to the HostManager on a specific container
+        to get the host metrics given timestamps
+
+        :param ip: the ip of the container
+        :param port: the port of the host manager
+        :param failed_auth_last_ts: the timestamp to read the last failed login attempts from
+        :param login_last_ts: the timestamp to read the last successful login attempts from
+        :return: the host metrics
+        """
+        # Open a gRPC session
+        with grpc.insecure_channel(f'{ip}:{port}') as channel:
+            stub = csle_collector.host_manager.host_manager_pb2_grpc.HostManagerStub(channel)
+            host_metrics_data = csle_collector.host_manager.query_host_manager.get_host_metrics(
+                stub=stub, failed_auth_last_ts=failed_auth_last_ts, login_last_ts=login_last_ts)
+            return host_metrics_data
 
     @staticmethod
     def get_host_managers_ips(emulation_env_config: EmulationEnvConfig) -> List[str]:
@@ -347,8 +457,17 @@ class HostController:
         :return: the list of IP addresses
         """
         ips = []
+
+        # Get ips of emulation containers
         for c in emulation_env_config.containers_config.containers:
             ips.append(c.get_ips()[0])
+
+        # Get ip of Kafka container
+        ips.append(emulation_env_config.kafka_config.container.get_ips()[0])
+
+        # Get ip of ELK container
+        ips.append(emulation_env_config.elk_config.container.get_ips()[0])
+
         return ips
 
     @staticmethod
@@ -360,8 +479,17 @@ class HostController:
         :return: the list of ports
         """
         ports = []
+
+        # Get port of emulation containers
         for c in emulation_env_config.containers_config.containers:
             ports.append(emulation_env_config.host_manager_config.host_manager_port)
+
+        # Get port of kafka container
+        ports.append(emulation_env_config.host_manager_config.host_manager_port)
+
+        # Get port of ELK container
+        ports.append(emulation_env_config.host_manager_config.host_manager_port)
+
         return ports
 
     @staticmethod
