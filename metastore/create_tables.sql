a552b5241490922a890eecc972a362b6ad51aadd
Kim Hammar <kimham@kth.se>
2023-02-10 20:03:31 +0100

    Citus [WIP]

--- metastore/create_tables.sql
+++ metastore/create_tables.sql
@@ -1,21 +1,6 @@
---  Create csle db if not exists --
-SELECT 'CREATE DATABASE csle'
-    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'csle')\gexec
-
 -- Connect to the csle db --
 \connect csle
 
--- Create csle user --
-REASSIGN OWNED BY csle TO postgres;
-DROP OWNED BY csle;
-DROP USER IF EXISTS csle;
-CREATE USER csle WITH ENCRYPTED PASSWORD 'csle';
-
--- Grant priviliges to csle user for the csle db --
-GRANT ALL PRIVILEGES ON DATABASE csle TO csle;
-
--- Create CITUS extension --
-CREATE EXTENSION citus;
 
 -- Setup citus coordinator node --
 SELECT citus_set_coordinator_host('172.31.212.92', 5432);
