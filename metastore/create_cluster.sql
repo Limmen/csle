4dd0a1548ca34f40e95c60add37cf69f7ff2937a
Limmen <kimham@kth.se>
2023-02-11 12:05:06 +0100

    upd cluster

--- metastore/create_cluster.sql
+++ metastore/create_cluster.sql
@@ -6,3 +6,4 @@ SELECT citus_set_coordinator_host('172.31.212.92', 5432);
 
 -- Setup citus worker nodes --
 SELECT citus_add_node('172.31.212.91', 5432);
+SELECT citus_add_node('172.31.212.93', 5432);
