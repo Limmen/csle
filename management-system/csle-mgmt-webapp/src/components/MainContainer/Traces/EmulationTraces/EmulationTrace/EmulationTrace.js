433cb299857f33a71aed481d02920456d1a18eae
Limmen <kimham@kth.se>
2023-03-26 18:11:34 +0200

    Level 13 [WIP]

--- management-system/csle-mgmt-webapp/src/components/MainContainer/Traces/EmulationTraces/EmulationTrace/EmulationTrace.js
+++ management-system/csle-mgmt-webapp/src/components/MainContainer/Traces/EmulationTraces/EmulationTrace/EmulationTrace.js
@@ -7,16 +7,59 @@ import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
 import './EmulationTrace.css';
 import Collapse from 'react-bootstrap/Collapse'
+import Select from 'react-select'
 import convertListToCommaSeparatedString from "../../../../Common/convertListToCommaSeparatedString";
 
 /**
  * Component representing the /emulation-traces/id resource
  */
 const EmulationTrace = (props) => {
+    const defMachines = props.emulationTrace.initial_defender_observation_state.machines.map((machine, index) => {
+        return {
+            value: machine,
+            label: `IPs: ${machine.ips.join(', ')}`
+        }
+    })
     const [attackerActionsOpen, setAttackerActionsOpen] = useState(false);
     const [defenderActionsOpen, setDefenderActionsOpen] = useState(false);
     const [attackerObservationsOpen, setAttackerObservationsOpen] = useState(false);
     const [defenderObservationsOpen, setDefenderObservationsOpen] = useState(false);
+    const [selectedDefenderMachine, setSelectedDefenderMachine] = useState(defMachines[0]);
+    const [defenderMachines, setDefenderMachines] = useState(defMachines);
+
+    const updateSelectedDefenderMachine = (selectedDefMachine) => {
+        setSelectedDefenderMachine(selectedDefMachine)
+    }
+
+    const getDefenderMachineObs = (defenderObsState) => {
+        for (let i = 0; i < defenderObsState.machines.length; i++) {
+            if(defenderObsState.machines[i].ips.includes(selectedDefenderMachine.value.ips[0])) {
+                return defenderObsState.machines[i]
+            }
+        }
+    }
+
+    const SelectDefenderMachine = (props) => {
+        return (
+            <div className="inline-block">
+                <div className="conditionalDist inline-block">
+                    <div className="conditionalDist inline-block conditionalLabel">
+                        Machine:
+                    </div>
+                    <div className="conditionalDist inline-block" style={{width: "300px"}}>
+                        <Select
+                            style={{display: 'inline-block'}}
+                            value={props.selectedDefenderMachine}
+                            defaultValue={props.selectedDefenderMachine}
+                            options={props.defenderMachines}
+                            onChange={updateSelectedDefenderMachine}
+                            placeholder="Select machine"
+                        />
+                    </div>
+                </div>
+            </div>
+        )
+    }
 
     const renderRemoveEmulationTraceTooltip = (props) => (
         <Tooltip id="button-tooltip" {...props} className="toolTipRefresh">
@@ -101,7 +144,7 @@ const EmulationTrace = (props) => {
     }
 
     const RenderActions = (props) => {
-        if(props.sessionData === null || props.sessionData === undefined || !props.sessionData.admin){
+        if (props.sessionData === null || props.sessionData === undefined || !props.sessionData.admin) {
             return (<></>)
         }
         return (
@@ -132,7 +175,7 @@ const EmulationTrace = (props) => {
         <Accordion.Collapse eventKey={props.emulationTrace.id}>
             <Card.Body>
                 <RenderActions sessionData={props.sessionData} removeEmulationTrace={props.removeEmulationTrace}
-                               emulationTrace={props.emulationTrace} />
+                               emulationTrace={props.emulationTrace}/>
                 <Card className="subCard">
                     <Card.Header>
                         <Button
@@ -293,7 +336,8 @@ const EmulationTrace = (props) => {
                         </Button>
                     </Card.Header>
                     <Collapse in={defenderObservationsOpen}>
-                        <div id="attackerActionsBody" className="cardBodyHidden">
+                        <div id="defenderActionsBody" className="cardBodyHidden">
+                            <h3> Aggregate observations </h3>
                             <div className="table-responsive">
                                 <Table striped bordered hover>
                                     <thead>
@@ -348,6 +392,66 @@ const EmulationTrace = (props) => {
                                     </tbody>
                                 </Table>
                             </div>
+                            <h3>Machine observations</h3>
+                            <SelectDefenderMachine selectedDefenderMachine={selectedDefenderMachine}
+                                                   defenderMachines={defenderMachines}
+                            />
+
+                            <div className="table-responsive">
+                                <Table striped bordered hover>
+                                    <thead>
+                                    <tr>
+                                        <th>t</th>
+                                        <th># Failed logins</th>
+                                        <th># Logged in users</th>
+                                        <th># Successful logins</th>
+                                        <th># Open TCP connections</th>
+                                        <th># User sessions</th>
+                                        <th># Block read</th>
+                                        <th># Block written</th>
+                                        <th># CPU utilization %</th>
+                                        <th># Memory utilization %</th>
+                                        <th># Received MB</th>
+                                        <th># Transmitted MB</th>
+                                        <th># PIDs</th>
+                                        <th>Snort Alerts weighted by priority</th>
+                                        <th># Snort Severe alerts</th>
+                                        <th># Snort Warning alerts</th>
+                                        <th>OSSEC Alerts weighted by level</th>
+                                        <th># OSSEC Severe alerts</th>
+                                        <th># OSSEC Warning alerts</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    {props.emulationTrace.defender_observation_states.map((obs_state, index) => {
+                                        var machineObs = getDefenderMachineObs(obs_state)
+                                            return (<tr key={index}>
+                                                <td>{index + 1}</td>
+                                                <td>{machineObs.host_metrics.num_failed_login_attempts}</td>
+                                                <td>{machineObs.host_metrics.num_logged_in_users}</td>
+                                                <td>{machineObs.host_metrics.num_login_events}</td>
+                                                <td>{machineObs.host_metrics.num_open_connections}</td>
+                                                <td>{machineObs.host_metrics.num_users}</td>
+                                                <td>{machineObs.docker_stats.blk_read}</td>
+                                                <td>{machineObs.docker_stats.blk_write}</td>
+                                                <td>{machineObs.docker_stats.cpu_percent}</td>
+                                                <td>{machineObs.docker_stats.mem_percent}</td>
+                                                <td>{machineObs.docker_stats.net_rx}</td>
+                                                <td>{machineObs.docker_stats.net_tx}</td>
+                                                <td>{machineObs.docker_stats.pids}</td>
+                                                <td>{machineObs.snort_ids_ip_alert_counters.alerts_weighted_by_priority}</td>
+                                                <td>{machineObs.snort_ids_ip_alert_counters.severe_alerts}</td>
+                                                <td>{machineObs.snort_ids_ip_alert_counters.warning_alerts}</td>
+                                                <td>{machineObs.snort_ids_ip_alert_counters.alerts_weighted_by_level}</td>
+                                                <td>{machineObs.snort_ids_ip_alert_counters.severe_alerts}</td>
+                                                <td>{machineObs.snort_ids_ip_alert_counters.warning_alerts}</td>
+                                            </tr>)
+                                        }
+                                    )}
+                                    </tbody>
+                                </Table>
+                            </div>
+
                         </div>
                     </Collapse>
                 </Card>
