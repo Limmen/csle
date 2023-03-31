366679ccd476be9d0aaa6f91ef3a838dd5c2945d
Kim Hammar <kimham@kth.se>
2023-03-31 14:57:22 +0200

    Update client population

--- emulation-system/envs/010/level_9/config.py
+++ emulation-system/envs/010/level_9/config.py
@@ -4020,14 +4020,14 @@ def default_traffic_config(network_id: int, time_step_len_seconds: int) -> Traff
         )],
         ip=f"{constants.CSLE.CSLE_SUBNETMASK_PREFIX}{network_id}."
            f"{collector_constants.EXTERNAL_NETWORK.NETWORK_ID_THIRD_OCTET}.254",
-        client_process_type=ClientPopulationProcessType.SINE_MODULATED_POISSON,
+        client_process_type=ClientPopulationProcessType.SPIKING,
         lamb=20, mu=4, client_manager_port=collector_constants.MANAGER_PORTS.CLIENT_MANAGER_DEFAULT_PORT,
         num_commands=2, client_time_step_len_seconds=time_step_len_seconds,
         time_scaling_factor=float(2/30), period_scaling_factor=20,
         client_manager_log_dir=collector_constants.LOG_FILES.CLIENT_MANAGER_LOG_DIR,
         client_manager_log_file=collector_constants.LOG_FILES.CLIENT_MANAGER_LOG_FILE,
         client_manager_max_workers=collector_constants.GRPC_WORKERS.DEFAULT_MAX_NUM_WORKERS,
-        exponents=None, factors=None, breakpoints=None, breakvalues=None)
+        exponents=[3, 7, 11, 16], factors=[10,40,30,20], breakpoints=None, breakvalues=None)
     traffic_conf = TrafficConfig(node_traffic_configs=traffic_generators,
                                  client_population_config=client_population_config)
     return traffic_conf
