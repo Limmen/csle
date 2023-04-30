a943007daf9123dbe6eef5e49de2f48c1612849d
Limmen <kimham@kth.se>
2023-04-30 14:43:35 +0200

    release 0.2.0

--- simulation-system/libs/make_release.py
+++ simulation-system/libs/make_release.py
@@ -5,40 +5,40 @@ import subprocess
 
 RELEASE_CONFIG = {
     "csle-ryu": {
-        "new_version": "'0.1.28'"
+        "new_version": "'0.2.0'"
     },
     "csle-collector": {
-        "new_version": "'0.1.28'"
+        "new_version": "'0.2.0'"
     },
     "csle-common": {
-        "new_version": "'0.1.28'"
+        "new_version": "'0.2.0'"
     },
     "csle-attacker": {
-        "new_version": "'0.1.28'"
+        "new_version": "'0.2.0'"
     },
     "csle-defender": {
-        "new_version": "'0.1.28'"
+        "new_version": "'0.2.0'"
     },
     "csle-system-identification": {
-        "new_version": "'0.1.28'"
+        "new_version": "'0.2.0'"
     },
     "gym-csle-stopping-game": {
-        "new_version": "'0.1.28'"
+        "new_version": "'0.2.0'"
     },
     "gym-csle-intrusion-response-game": {
-        "new_version": "'0.1.28'"
+        "new_version": "'0.2.0'"
     },
     "csle-agents": {
-        "new_version": "'0.1.28'"
+        "new_version": "'0.2.0'"
     },
     "csle-rest-api": {
-        "new_version": "'0.1.28'"
+        "new_version": "'0.2.0'"
     },
     "csle-cli": {
-        "new_version": "'0.1.28'"
+        "new_version": "'0.2.0'"
     },
     "csle-cluster": {
-        "new_version": "'0.1.28'"
+        "new_version": "'0.2.0'"
     }
 }
 
