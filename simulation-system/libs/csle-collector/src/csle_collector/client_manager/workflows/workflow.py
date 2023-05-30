c06ef2e31f9120e6cc599c358a36a6169bb9897b
Limmen <kimham@kth.se>
2023-05-30 11:26:13 +0200

    cleanup

--- simulation-system/libs/csle-collector/src/csle_collector/client_manager/workflows/workflow.py
+++ simulation-system/libs/csle-collector/src/csle_collector/client_manager/workflows/workflow.py
@@ -1,13 +1,19 @@
 import random
 from typing import List
-
 import numpy as np
+
 
 class Workflow:
     """
     A Markov chain representation of a workflow.
     """
-    def __init__(self, transition_matrix: List[List[float]], initial_state: int):
+    def __init__(self, transition_matrix: List[List[float]], initial_state: int) -> None:
+        """
+        Initializes the object
+
+        :param transition_matrix: the transition matrix of the workflow Markov chain (row-stochastic)
+        :param initial_state: the initial state of the Markov chain
+        """
         self.transition_matrix = transition_matrix
         self.initial_state = initial_state
         self.current_state = initial_state
@@ -23,15 +29,22 @@ class Workflow:
         self.current_state = next_state
         return self.current_state
     
-    def reset(self):
+    def reset(self) -> None:
+        """
+        Resets the Markov chain
+
+        :return: None
+        """
         self.current_state = self.initial_state
         self.t = 0
 
-def markov_next_state(row):
-    """ 
-    This method was written by Bing AI.
-    Row is a numpy array of probabilities that sum up to 1.
-    Returns an integer from 0 to len(row)-1.
+
+def markov_next_state(row: List[float]) -> int:
+    """
+    Samples the next state of the Markov chain
+
+    :param row: a numpy array of probabilities that sum up to 1.
+    :return:  an integer from 0 to len(row)-1.
     """
     cumsum = np.cumsum(row) # cumulative sum of probabilities
     r = random.random() # random number between 0 and 1
--- simulation-system/libs/csle-collector/src/csle_collector/client_manager/workflows/workflow_distribution.py
+++ simulation-system/libs/csle-collector/src/csle_collector/client_manager/workflows/workflow_distribution.py
@@ -1,6 +1,5 @@
 from random import choices
 from typing import List, Tuple
-
 from csle_collector.client_manager.workflows.workflow import Workflow
 
 
@@ -8,13 +7,19 @@ class WorkflowDistribution:
     """
     A workflow distribution that represents a random variable whose outcomes consists of different workflows.
     """
-    def __init__(self, workflows: List[Tuple[float, Workflow]]):
+    def __init__(self, workflows: List[Tuple[float, Workflow]]) -> None:
+        """
+        Initializes the object
+
+        :param workflows: the list of workflows
+        """
         self.workflows = workflows
     
-    def sample(self):
+    def sample(self) -> Workflow:
         """
-        This method was written by Bing AI.
-        Returns a random workflow according to the distribution.
+        Samples a workflow from the distribution
+
+        :return: a sampled workflow
         """
         weights = [w[0] for w in self.workflows]
         workflows = [w[1] for w in self.workflows]
