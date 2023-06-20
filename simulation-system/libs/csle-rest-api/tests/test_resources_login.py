ea5f7ae291e860675ee8244b108ae38481d471b2
nforsg <forsgren.nils@gmail.com>
2023-06-20 11:46:58 +0200

    one function finished

--- simulation-system/libs/csle-rest-api/tests/test_resources_login.py
+++ simulation-system/libs/csle-rest-api/tests/test_resources_login.py
@@ -11,7 +11,7 @@ class TestResourcesLoginSuite(object):
     Test suite for /login resource
     """
 
-    pytest.logger = logging.getLogger("resources_version_tests")
+    pytest.logger = logging.getLogger("resources_login_tests")
 
     @pytest.fixture
     def flask_app(self):
@@ -22,16 +22,24 @@ class TestResourcesLoginSuite(object):
         """
         return create_app(static_folder="../../../../../management-system/csle-mgmt-webapp/build")
 
-    def test_login_resource(self, flask_app) -> None:
+    def test_successful_admin_login(self, flask_app) -> None:
         """
-        Tests the /login resource
-
-        :return: None
+        Tests the /login resource when logging in with 'admin' and 'admin as 
+        username- and password credentials
         """
         response = flask_app.test_client().post(api_constants.MGMT_WEBAPP.LOGIN_RESOURCE,
                                                 data=json.dumps({'username':'admin', 'password':'admin'}))
         response_data = response.data.decode("utf-8")
-        assert response.status_code == constants.HTTPS.OK_STATUS_CODE
+        response_data_dict = json.loads(response_data)
+        pytest.logger.info(response.status_code)
+        #pytest.logger.info(response.data.decode('utf-8'))
+        pytest.logger.info(response_data_dict)
+        assert response_data_dict[api_constants.MGMT_WEBAPP.ADMIN_PROPERTY] == True 
+        assert response_data_dict[api_constants.MGMT_WEBAPP.FIRST_NAME_PROPERTY] == api_constants.MGMT_WEBAPP.ADMIN_PROPERTY
+        assert response_data_dict[api_constants.MGMT_WEBAPP.USERNAME_PROPERTY] == api_constants.MGMT_WEBAPP.ADMIN_PROPERTY
+        
+        '''assert response.status_code == constants.HTTPS.OK_STATUS_CODE
+
         assert api_constants.MGMT_WEBAPP.ADMIN_PROPERTY in response_data
         assert api_constants.MGMT_WEBAPP.TOKEN_PROPERTY in response_data
         assert api_constants.MGMT_WEBAPP.USERNAME_PROPERTY in response_data
@@ -39,4 +47,19 @@ class TestResourcesLoginSuite(object):
         assert api_constants.MGMT_WEBAPP.LAST_NAME_PROPERTY in response_data
         assert api_constants.MGMT_WEBAPP.ORGANIZATION_PROPERTY in response_data
         assert api_constants.MGMT_WEBAPP.EMAIL_PROPERTY in response_data
-        assert api_constants.MGMT_WEBAPP.ID_PROPERTY in response_data        
+        assert api_constants.MGMT_WEBAPP.ID_PROPERTY in response_data'''        
+
+
+        def test_successgful_guest_login(self, flask_app):
+            '''Tests the /login resource when logging in with 'guest' username- and password credentials'''
+            response = flask_app.test_client().post(api_constants.MGMT_WEBAPP.LOGIN_RESOURCE,
+                                                data=json.dumps({'username':'admin', 'password':'admin'}))
+        
+
+
+
+        '''response = flask_app.test_client().post(api_constants.MGMT_WEBAPP.LOGIN_RESOURCE,
+                                                data=json.dumps({'username':'guest', 'password':'guest'}))
+        response_data = response.data.decode("utf-8")
+        pytest.logger.info(response.status_code)
+        pytest.logger.info(len(response_data))'''
