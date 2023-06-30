bde190c30b3220f4a409c3424de1ca1a635f88d1
nforsg <forsgren.nils@gmail.com>
2023-06-30 09:53:45 +0200

    finished testing

--- simulation-system/libs/csle-rest-api/src/csle_rest_api/resources/grafana/routes.py
+++ simulation-system/libs/csle-rest-api/src/csle_rest_api/resources/grafana/routes.py
@@ -2,13 +2,14 @@
 Routes and sub-resources for the /grafana resource
 """
 import json
-from flask import Blueprint, jsonify, request
+
 import csle_common.constants.constants as constants
+from csle_cluster.cluster_manager.cluster_controller import ClusterController
+from csle_common.metastore.metastore_facade import MetastoreFacade
+from flask import Blueprint, jsonify, request
+
 import csle_rest_api.constants.constants as api_constants
 import csle_rest_api.util.rest_api_util as rest_api_util
-from csle_common.metastore.metastore_facade import MetastoreFacade
-from csle_cluster.cluster_manager.cluster_controller import ClusterController
-
 
 # Creates a blueprint "sub application" of the main REST app
 grafana_bp = Blueprint(api_constants.MGMT_WEBAPP.GRAFANA_RESOURCE, __name__,
@@ -27,12 +28,13 @@ def grafana():
     authorized = rest_api_util.check_if_user_is_authorized(request=request, requires_admin=requires_admin)
     if authorized is not None:
         return authorized
-
-    json_data = json.loads(request.data)
-    if api_constants.MGMT_WEBAPP.IP_PROPERTY not in json_data:
-        return jsonify({}), constants.HTTPS.BAD_REQUEST_STATUS_CODE
-    ip = json_data[api_constants.MGMT_WEBAPP.IP_PROPERTY]
-
+    if request.method == api_constants.MGMT_WEBAPP.HTTP_REST_POST:
+        json_data = json.loads(request.data)
+        if api_constants.MGMT_WEBAPP.IP_PROPERTY not in json_data:
+            return jsonify({}), constants.HTTPS.BAD_REQUEST_STATUS_CODE
+        ip = json_data[api_constants.MGMT_WEBAPP.IP_PROPERTY]
+    else:
+        ip = " "
     config = MetastoreFacade.get_config(id=1)
     cluster_statuses = []
     for node in config.cluster_config.cluster_nodes:
