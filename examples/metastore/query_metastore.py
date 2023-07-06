d87046ed2eda18df50cdf6113d989e9fe89e8958
Limmen <kimham@kth.se>
2023-07-06 18:41:12 +0200

    add example of querying the metastore

---
+++ examples/metastore/query_metastore.py
@@ -0,0 +1,26 @@
+from typing import List, Dict
+import psycopg
+import csle_common.constants.constants as constants
+
+
+def query_metastore_select(table: str) -> List[Dict[int, Dict]]:
+    """
+    Performs a SELECT * query for a given table in the metastore
+
+    :param table: the name of the table to select data  from
+    :return: the selected records
+    """
+    with psycopg.connect(f"{constants.METADATA_STORE.DB_NAME_PROPERTY}={constants.METADATA_STORE.DBNAME} "
+                         f"{constants.METADATA_STORE.USER_PROPERTY}={constants.METADATA_STORE.USER} "
+                         f"{constants.METADATA_STORE.PW_PROPERTY}={constants.METADATA_STORE.PASSWORD} "
+                         f"{constants.METADATA_STORE.HOST_PROPERTY}={constants.METADATA_STORE.HOST}") as conn:
+        with conn.cursor() as cur:
+            cur.execute(f"SELECT * FROM {table}")
+            records = cur.fetchall()
+            return records
+
+
+if __name__ == '__main__':
+    example_table = constants.METADATA_STORE.PPO_POLICIES_TABLE
+    records = query_metastore_select(table=example_table)
+    print(records)
