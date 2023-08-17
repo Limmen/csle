6fc0754c935be1150fba630ba81acc3bc182703a
Kim Hammar <kimham@kth.se>
2023-08-17 16:55:33 +0200

    Unit tests

--- simulation-system/libs/gym-csle-intrusion-response-game/tests/test_intrusion_response_game_util.py
+++ simulation-system/libs/gym-csle-intrusion-response-game/tests/test_intrusion_response_game_util.py
@@ -1,3 +1,4 @@
+from gym_csle_intrusion_response_game.util.intrusion_response_game_util import IntrusionResponseGameUtil
 
 
 class TestIntrusionResponseGameUtilSuite(object):
@@ -5,10 +6,13 @@ class TestIntrusionResponseGameUtilSuite(object):
     Test suite for stopping_game_util.py
     """
 
-    def test_b1(self) -> None:
+    def test_is_local_state_terminal(self) -> None:
         """
-        Tests the b1 function
+        Tests the is_local_state_terminal function
 
         :return: None
         """
-        assert True
+        assert IntrusionResponseGameUtil.is_local_state_terminal([-1, -1])
+        assert not IntrusionResponseGameUtil.is_local_state_terminal([0, -1])
+        assert not IntrusionResponseGameUtil.is_local_state_terminal([-1, 0])
+        assert not IntrusionResponseGameUtil.is_local_state_terminal([0, 0])
