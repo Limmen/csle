6d07e59f0d8abb7ca7633364801d4d7b76a73f25
Kim Hammar <kimham@kth.se>
2023-08-27 21:38:10 +0200

    fix linter

--- simulation-system/libs/csle-common/tests/test_emulation_observation_dao.py
+++ simulation-system/libs/csle-common/tests/test_emulation_observation_dao.py
@@ -1,8 +1,8 @@
 from csle_common.dao.emulation_observation.common.emulation_port_observation_state import EmulationPortObservationState
 from csle_common.dao.emulation_config.transport_protocol import TransportProtocol
-from csle_common.dao.emulation_observation.attacker.\
+from csle_common.dao.emulation_observation.attacker. \
     emulation_attacker_machine_observation_state import EmulationAttackerMachineObservationState
-from csle_common.dao.emulation_observation.attacker.\
+from csle_common.dao.emulation_observation.attacker. \
     emulation_attacker_observation_state import EmulationAttackerObservationState
 from csle_common.dao.emulation_observation.common.emulation_connection_observation_state \
     import EmulationConnectionObservationState
@@ -20,11 +20,9 @@ class TestEmulationObservationDaoSuite:
 
         :return: None
         """
-
         emulation_port_observation_state = EmulationPortObservationState(
             port=3333, open=False, service="myservice", protocol=TransportProtocol.TCP, http_enum="testenum",
             http_grep="testgrep", vulscan="vulscantest", version="myversion", fingerprint="myfp")
-
         assert isinstance(emulation_port_observation_state.to_dict(), dict)
         assert isinstance(EmulationPortObservationState.from_dict(emulation_port_observation_state.to_dict()),
                           EmulationPortObservationState)
@@ -35,14 +33,12 @@ class TestEmulationObservationDaoSuite:
 
     def test_emulation_attacker_machine_observation_state(self) -> None:
         """
-                Tests creation and dict conversion of the EmulationAttackerMachineObservationState DAO
+        Tests creation and dict conversion of the EmulationAttackerMachineObservationState DAO
 
-                :return: None
-                """
-
+        :return: None
+        """
         emulation_attack_machine_observation_state = (EmulationAttackerMachineObservationState
                                                       (ips=["172.31.212.1", "172.31.212.2"]))
-
         assert isinstance(emulation_attack_machine_observation_state.to_dict(), dict)
 
         assert isinstance(EmulationAttackerMachineObservationState.from_dict
@@ -57,16 +53,13 @@ class TestEmulationObservationDaoSuite:
 
     def test_emulation_attacker_observation_state(self) -> None:
         """
-                Tests creation and dict conversion of the EmulationAttackerObservationState DAO
+        Tests creation and dict conversion of the EmulationAttackerObservationState DAO
 
-                :return: None
-                """
-
+        :return: None
+        """
         emulation_attack_observation_state = (EmulationAttackerObservationState
-                                              (catched_flags=1, agent_reachable=set(["test1", "test2"])))
-
+                                              (catched_flags=1, agent_reachable={"test1", "test2"}))
         assert isinstance(emulation_attack_observation_state.to_dict(), dict)
-
         assert isinstance(EmulationAttackerObservationState.from_dict
                           (emulation_attack_observation_state.to_dict()),
                           EmulationAttackerObservationState)
@@ -79,16 +72,14 @@ class TestEmulationObservationDaoSuite:
 
     def test_emulation_connection_observation_state(self) -> None:
         """
-                Tests creation and dict conversion of the EmulationConnectionObservationState DAO
+        Tests creation and dict conversion of the EmulationConnectionObservationState DAO
 
-                :return: None
-                """
+        :return: None
+        """
         emulation_connection_observation_state = (EmulationConnectionObservationState
                                                   (conn=None, credential=Credential(username="shahab", pw="123"),
                                                    root=False, service="test1", port=123))
-
         assert isinstance(emulation_connection_observation_state.to_dict(), dict)
-
         assert isinstance(EmulationConnectionObservationState.from_dict
                           (emulation_connection_observation_state.to_dict()),
                           EmulationConnectionObservationState)
