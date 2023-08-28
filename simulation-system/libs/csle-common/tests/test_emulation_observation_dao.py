aa6a93d355da450d52084ee0c8310f1fca812a4c
foroogh shahab <fo.shahab@gmail.com>
2023-08-28 11:00:18 +0200

    add unit test for csle-common

--- simulation-system/libs/csle-common/tests/test_emulation_observation_dao.py
+++ simulation-system/libs/csle-common/tests/test_emulation_observation_dao.py
@@ -9,6 +9,20 @@ from csle_common.dao.emulation_observation.common.emulation_connection_observati
 from csle_common.dao.emulation_config.credential import Credential
 from csle_common.dao.emulation_observation.common.emulation_vulnerability_observation_state \
     import EmulationVulnerabilityObservationState
+from csle_common.dao.emulation_observation.defender.emulation_defender_machine_observation_state import \
+    EmulationDefenderMachineObservationState
+from csle_common.dao.emulation_config.kafka_config import KafkaConfig
+from csle_common.dao.emulation_config.node_container_config import NodeContainerConfig
+from csle_common.dao.emulation_config.node_resources_config import NodeResourcesConfig
+from csle_common.dao.emulation_config.node_firewall_config import NodeFirewallConfig
+from csle_common.dao.emulation_config.kafka_topic import KafkaTopic
+from csle_common.dao.emulation_config.container_network import ContainerNetwork
+from csle_common.dao.emulation_config.node_network_config import NodeNetworkConfig
+from csle_common.dao.emulation_config.default_network_firewall_config import DefaultNetworkFirewallConfig
+from csle_collector.host_manager.dao.host_metrics import HostMetrics
+from csle_collector.docker_stats_manager.dao.docker_stats import DockerStats
+from csle_collector.snort_ids_manager.dao.snort_ids_ip_alert_counters import SnortIdsIPAlertCounters
+from csle_collector.ossec_ids_manager.dao.ossec_ids_alert_counters import OSSECIdsAlertCounters
 
 
 class TestEmulationObservationDaoSuite:
@@ -122,3 +136,58 @@ class TestEmulationObservationDaoSuite:
         assert (EmulationVulnerabilityObservationState.from_dict
                 (emulation_vulnerability_observation_state.to_dict()) ==
                 emulation_vulnerability_observation_state)
+
+    def test_emulation_defender_machine_observation_state(self) -> None:
+        """
+        Tests creation and dict conversion of the EmulationConnectionObservationState DAO
+
+        :return: None
+        """
+
+        ips = ["10.10.10.10", "20.20.20.20"]
+        container_network = ContainerNetwork(name="test1", subnet_mask="0.0.0.0",
+                                             bitmask="0.0.0.0", subnet_prefix="0.0.0.0")
+        ips_and_networks = [("container_network_test", container_network)]
+        container = NodeContainerConfig("test1", ips_and_networks=ips_and_networks, version="1.0", level="1",
+                                        restart_policy="test1", suffix="test2", os="ubuntu")
+        ips_and_network_configs = [("test2", NodeNetworkConfig())]
+        resources = NodeResourcesConfig(container_name="test1", num_cpus=1,
+                                        available_memory_gb=100, ips_and_network_configs=ips_and_network_configs)
+
+        default_network_firewall_config = DefaultNetworkFirewallConfig(ip="1.1.1.1",
+                                                                       default_gw="2.3.3.4",
+                                                                       default_input="test",
+                                                                       default_output="tets",
+                                                                       default_forward="test",
+                                                                       network=container_network)
+        ips_gw_default_policy_networks = [default_network_firewall_config]
+        firewall_config = NodeFirewallConfig(ips_gw_default_policy_networks=ips_gw_default_policy_networks,
+                                             hostname="test1", output_accept={"test1", "test2"},
+                                             forward_accept={"test1", "test2"}, output_drop={"test"},
+                                             input_drop={"test2"}, forward_drop={"test3"},
+                                             input_accept=set(["test"]),
+                                             routes={("test", "test")})
+        topics = [
+            KafkaTopic(name="test", num_partitions=1, num_replicas=2, attributes=["test"], retention_time_hours=1)]
+
+        kafka_config = KafkaConfig(container=container, resources=resources, firewall_config=firewall_config,
+                                   topics=topics, kafka_manager_log_file="test", kafka_manager_log_dir="test",
+                                   kafka_manager_max_workers=10)
+
+        emulation_defender_machine_observation_state = (EmulationDefenderMachineObservationState
+                                                        (ips=ips, kafka_config=kafka_config, host_metrics=HostMetrics(),
+                                                         docker_stats=DockerStats(),
+                                                         snort_ids_ip_alert_counters=SnortIdsIPAlertCounters(),
+                                                         ossec_ids_alert_counters=OSSECIdsAlertCounters()))
+
+        assert isinstance(emulation_defender_machine_observation_state.to_dict(), dict)
+
+        assert isinstance(EmulationDefenderMachineObservationState.from_dict
+                          (emulation_defender_machine_observation_state.to_dict()),
+                          EmulationDefenderMachineObservationState)
+        assert (EmulationDefenderMachineObservationState.from_dict
+                (emulation_defender_machine_observation_state.to_dict()).to_dict() ==
+                emulation_defender_machine_observation_state.to_dict())
+        assert (EmulationDefenderMachineObservationState.from_dict
+                (emulation_defender_machine_observation_state.to_dict()) ==
+                emulation_defender_machine_observation_state)
