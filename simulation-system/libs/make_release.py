8c4e1d494a4535cb9d3673b61408330f896ef07c
Kim Hammar <kimham@kth.se>
2023-08-30 10:03:21 +0200

    Release Python 0.3.8

--- simulation-system/libs/make_release.py
+++ simulation-system/libs/make_release.py
@@ -5,43 +5,43 @@ import subprocess
 
 RELEASE_CONFIG = {
     "csle-base": {
-        "new_version": "'0.3.7'"
+        "new_version": "'0.3.8'"
     },
     "csle-ryu": {
-        "new_version": "'0.3.7'"
+        "new_version": "'0.3.8'"
     },
     "csle-collector": {
-        "new_version": "'0.3.7'"
+        "new_version": "'0.3.8'"
     },
     "csle-common": {
-        "new_version": "'0.3.7'"
+        "new_version": "'0.3.8'"
     },
     "csle-attacker": {
-        "new_version": "'0.3.7'"
+        "new_version": "'0.3.8'"
     },
     "csle-defender": {
-        "new_version": "'0.3.7'"
+        "new_version": "'0.3.8'"
     },
     "csle-system-identification": {
-        "new_version": "'0.3.7'"
+        "new_version": "'0.3.8'"
     },
     "gym-csle-stopping-game": {
-        "new_version": "'0.3.7'"
+        "new_version": "'0.3.8'"
     },
     "gym-csle-intrusion-response-game": {
-        "new_version": "'0.3.7'"
+        "new_version": "'0.3.8'"
     },
     "csle-agents": {
-        "new_version": "'0.3.7'"
+        "new_version": "'0.3.8'"
     },
     "csle-rest-api": {
-        "new_version": "'0.3.7'"
+        "new_version": "'0.3.8'"
     },
     "csle-cli": {
-        "new_version": "'0.3.7'"
+        "new_version": "'0.3.8'"
     },
     "csle-cluster": {
-        "new_version": "'0.3.7'"
+        "new_version": "'0.3.8'"
     }
 }
 
