5c0686468da8185d77d6b2479542f6d6a2c1edd2
foroogh shahab <fo.shahab@gmail.com>
2023-09-13 11:31:57 +0200

    add unit test for csle-common

--- simulation-system/libs/csle-common/tests/test_metastore_facade.py
+++ simulation-system/libs/csle-common/tests/test_metastore_facade.py
@@ -760,3 +760,32 @@ class TestMetastoreFacadeSuite:
         assert isinstance(simulation_traces_ids[0][0], int)
         assert isinstance(simulation_traces_ids[0][1], str)
         assert simulation_traces_ids[0] == example_record
+
+    def test_list_simulation_traces(self, mocker: pytest_mock.MockFixture,
+                                    example_simulation_trace: SimulationTrace) -> None:
+        """
+        Tests the list_simulation_traces function
+
+        :param mocker: the pytest mocker object
+        :param example_simulation_traces: an example SimulationTrace object
+        :return: None
+        """
+        id = 1
+        example_simulation_trace.id = id
+        example_record = (id, example_simulation_trace.simulation_env, example_simulation_trace.to_dict())
+        mocked_connection = mocker.MagicMock()
+        mocked_cursor = mocker.MagicMock()
+        mocker.patch('psycopg.connect', return_value=mocked_connection)
+        mocked_connection.configure_mock(**{"__enter__.return_value": mocked_connection})
+        mocked_connection.configure_mock(**{"cursor.return_value": mocked_cursor})
+        mocked_cursor.configure_mock(**{"execute.return_value": None})
+        mocked_cursor.configure_mock(**{"fetchall.return_value": [example_record]})
+        mocked_cursor.configure_mock(**{"__enter__.return_value": mocked_cursor})
+        simulation_traces = MetastoreFacade.list_simulation_traces()
+        mocked_connection.cursor.assert_called_once()
+        mocked_cursor.execute.assert_called_once_with(
+            f"SELECT * FROM {constants.METADATA_STORE.SIMULATION_TRACES_TABLE}")
+        mocked_cursor.fetchall.assert_called_once()
+        assert isinstance(simulation_traces, list)
+        assert isinstance(simulation_traces[0], SimulationTrace)
+        assert simulation_traces[0] == example_simulation_trace
