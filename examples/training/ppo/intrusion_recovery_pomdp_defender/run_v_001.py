71b76ff734295a6ee594b1d25190b9ec250dd853
Kim Hammar <kimham@kth.se>
2023-10-02 08:53:16 +0200

    PPO intrusion recovery

---
+++ examples/training/ppo/intrusion_recovery_pomdp_defender/run_v_001.py
@@ -0,0 +1,129 @@
+import numpy as np
+import csle_common.constants.constants as constants
+from csle_common.dao.training.experiment_config import ExperimentConfig
+from csle_common.metastore.metastore_facade import MetastoreFacade
+from csle_common.dao.training.agent_type import AgentType
+from csle_common.dao.training.hparam import HParam
+from csle_common.dao.training.player_type import PlayerType
+from csle_agents.agents.ppo.ppo_agent import PPOAgent
+import csle_agents.constants.constants as agents_constants
+import gym_csle_intrusion_response_game.constants.constants as env_constants
+from gym_csle_intrusion_response_game.util.intrusion_response_game_util import IntrusionResponseGameUtil
+from gym_csle_intrusion_response_game.dao.local_intrusion_response_game_config import LocalIntrusionResponseGameConfig
+from csle_common.dao.training.tabular_policy import TabularPolicy
+from csle_tolerance.dao.intrusion_recovery_pomdp_config import IntrusionRecoveryPomdpConfig
+from csle_tolerance.util.intrusion_recovery_pomdp_util import IntrusionRecoveryPomdpUtil
+
+if __name__ == '__main__':
+    emulation_name = "csle-level9-030"
+    emulation_env_config = MetastoreFacade.get_emulation_by_name(emulation_name)
+    if emulation_env_config is None:
+        raise ValueError(f"Could not find an emulation environment with the name: {emulation_name}")
+    simulation_name = "csle-tolerance-intrusion-recovery-pomdp-defender-001"
+    simulation_env_config = MetastoreFacade.get_simulation_by_name(simulation_name)
+    if simulation_env_config is None:
+        raise ValueError(f"Could not find a simulation with name: {simulation_name}")
+    input_config: IntrusionRecoveryPomdpConfig = simulation_env_config.simulation_env_input_config
+    eta = 1
+    p_a = 0.1
+    p_c_1 = 0.00001
+    p_c_2 = 0.001
+    p_u = 0.02
+    BTR = 5
+    negate_costs = True
+    discount_factor = 1
+    num_observations = 1000
+    cost_tensor = IntrusionRecoveryPomdpUtil.cost_tensor(eta=eta, states=IntrusionRecoveryPomdpUtil.state_space(),
+                                                         actions=IntrusionRecoveryPomdpUtil.action_space(),
+                                                         negate=negate_costs)
+    observation_tensor = IntrusionRecoveryPomdpUtil.observation_tensor(
+        states=IntrusionRecoveryPomdpUtil.state_space(),
+        observations=IntrusionRecoveryPomdpUtil.observation_space(num_observations=num_observations))
+    transition_tensor = IntrusionRecoveryPomdpUtil.transition_tensor(
+        states=IntrusionRecoveryPomdpUtil.state_space(), actions=IntrusionRecoveryPomdpUtil.action_space(), p_a=p_a,
+        p_c_1=p_c_1, p_c_2=p_c_2, p_u=p_u)
+    input_config = IntrusionRecoveryPomdpConfig(
+        eta=eta, p_a=p_a, p_c_1=p_c_1, p_c_2=p_c_2, p_u=p_u, BTR=BTR, negate_costs=negate_costs, seed=999,
+        discount_factor=discount_factor, states=IntrusionRecoveryPomdpUtil.state_space(),
+        actions=IntrusionRecoveryPomdpUtil.action_space(),
+        observations=IntrusionRecoveryPomdpUtil.observation_space(num_observations=num_observations),
+        cost_tensor=cost_tensor, observation_tensor=observation_tensor, transition_tensor=transition_tensor,
+        b1=IntrusionRecoveryPomdpUtil.initial_belief(p_a=p_a), T=BTR,
+        simulation_env_name=simulation_name, gym_env_name="csle-tolerance-intrusion-recovery-pomdp-v1"
+    )
+    simulation_env_config.simulation_env_input_config = input_config
+    L = 1
+    experiment_config = ExperimentConfig(
+        output_dir=f"{constants.LOGGING.DEFAULT_LOG_DIR}ppo_test",
+        title="PPO test",
+        random_seeds=[561512, 351, 5126, 2350, 16391, 52101, 3520210, 11124, 61912, 888812, 235610, 12511,
+                      44102, 21501, 5112, 35011, 7776612, 22212, 2019850, 98212, 333901],
+        agent_type=AgentType.PPO,
+        log_every=1,
+        hparams={
+            constants.NEURAL_NETWORKS.NUM_NEURONS_PER_HIDDEN_LAYER: HParam(
+                value=64, name=constants.NEURAL_NETWORKS.NUM_NEURONS_PER_HIDDEN_LAYER,
+                descr="neurons per hidden layer of the policy network"),
+            constants.NEURAL_NETWORKS.NUM_HIDDEN_LAYERS: HParam(
+                value=4, name=constants.NEURAL_NETWORKS.NUM_HIDDEN_LAYERS,
+                descr="number of layers of the policy network"),
+            agents_constants.PPO.STEPS_BETWEEN_UPDATES: HParam(
+                value=4096, name=agents_constants.PPO.STEPS_BETWEEN_UPDATES,
+                descr="number of steps in the environment for doing rollouts between policy updates"),
+            agents_constants.COMMON.BATCH_SIZE: HParam(value=64, name=agents_constants.COMMON.BATCH_SIZE,
+                                                       descr="batch size for updates"),
+            agents_constants.COMMON.LEARNING_RATE: HParam(value=0.00001,
+                                                          name=agents_constants.COMMON.LEARNING_RATE,
+                                                          descr="learning rate for updating the policy"),
+            constants.NEURAL_NETWORKS.DEVICE: HParam(value="cpu",
+                                                     name=constants.NEURAL_NETWORKS.DEVICE,
+                                                     descr="the device to train on (cpu or cuda:x)"),
+            agents_constants.COMMON.NUM_PARALLEL_ENVS: HParam(
+                value=1, name=agents_constants.COMMON.NUM_PARALLEL_ENVS,
+                descr="the nunmber of parallel environments for training"),
+            agents_constants.COMMON.GAMMA: HParam(
+                value=1, name=agents_constants.COMMON.GAMMA, descr="the discount factor"),
+            agents_constants.PPO.GAE_LAMBDA: HParam(
+                value=0.95, name=agents_constants.PPO.GAE_LAMBDA, descr="the GAE weighting term"),
+            agents_constants.PPO.CLIP_RANGE: HParam(
+                value=0.2, name=agents_constants.PPO.CLIP_RANGE, descr="the clip range for PPO"),
+            agents_constants.PPO.CLIP_RANGE_VF: HParam(
+                value=None, name=agents_constants.PPO.CLIP_RANGE_VF,
+                descr="the clip range for PPO-update of the value network"),
+            agents_constants.PPO.ENT_COEF: HParam(
+                value=0.0, name=agents_constants.PPO.ENT_COEF,
+                descr="the entropy coefficient for exploration"),
+            agents_constants.PPO.VF_COEF: HParam(value=0.5, name=agents_constants.PPO.VF_COEF,
+                                                 descr="the coefficient of the value network for the loss"),
+            agents_constants.PPO.MAX_GRAD_NORM: HParam(
+                value=0.5, name=agents_constants.PPO.MAX_GRAD_NORM, descr="the maximum allows gradient norm"),
+            agents_constants.PPO.TARGET_KL: HParam(value=None,
+                                                   name=agents_constants.PPO.TARGET_KL,
+                                                   descr="the target kl"),
+            agents_constants.COMMON.NUM_TRAINING_TIMESTEPS: HParam(
+                value=int(600000), name=agents_constants.COMMON.NUM_TRAINING_TIMESTEPS,
+                descr="number of timesteps to train"),
+            agents_constants.COMMON.EVAL_EVERY: HParam(value=1, name=agents_constants.COMMON.EVAL_EVERY,
+                                                       descr="training iterations between evaluations"),
+            agents_constants.COMMON.EVAL_BATCH_SIZE: HParam(value=100, name=agents_constants.COMMON.EVAL_BATCH_SIZE,
+                                                            descr="the batch size for evaluation"),
+            agents_constants.COMMON.SAVE_EVERY: HParam(value=10000, name=agents_constants.COMMON.SAVE_EVERY,
+                                                       descr="how frequently to save the model"),
+            agents_constants.COMMON.CONFIDENCE_INTERVAL: HParam(
+                value=0.95, name=agents_constants.COMMON.CONFIDENCE_INTERVAL,
+                descr="confidence interval"),
+            agents_constants.COMMON.MAX_ENV_STEPS: HParam(
+                value=500, name=agents_constants.COMMON.MAX_ENV_STEPS,
+                descr="maximum number of steps in the environment (for envs with infinite horizon generally)"),
+            agents_constants.COMMON.RUNNING_AVERAGE: HParam(
+                value=100, name=agents_constants.COMMON.RUNNING_AVERAGE,
+                descr="the number of samples to include when computing the running avg")
+        },
+        player_type=PlayerType.ATTACKER, player_idx=0
+    )
+    agent = PPOAgent(emulation_env_config=emulation_env_config, simulation_env_config=simulation_env_config,
+                     experiment_config=experiment_config, save_to_metastore=False)
+    experiment_execution = agent.train()
+    # MetastoreFacade.save_experiment_execution(experiment_execution)
+    # for policy in experiment_execution.result.policies.values():
+    #     MetastoreFacade.save_ppo_policy(ppo_policy=policy)
