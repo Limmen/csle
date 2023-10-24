5414a776047190bbf5871ec58ca490a74f530df0
Limmen <kimham@kth.se>
2023-10-24 14:24:08 +0000

    fix local install script

--- simulation-system/libs/local_install.sh
+++ simulation-system/libs/local_install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-cd csle-base; pip install -e .
-cd csle-collector; pip install -e .
-cd csle-ryu; pip install -e .
-cd csle-common; pip install -e .
-cd csle-cluster; pip install -e .
-cd csle-attacker; pip install -e .
-cd csle-defender; pip install -e .
-cd csle-gym-csle-intrusion-response-game; pip install -e .
-cd csle-system-identification; pip install -e .
-cd csle-gym-csle-stopping-game; pip install -e .
-cd csle-tolerance; pip install -e .
-cd csle-agents; pip install -e .
-cd csle-rest-api; pip install -e .
-cd csle-cli; pip install -e .
+cd csle-base; pip install -e .; cd ..
+cd csle-collector; pip install -e .; cd ..
+cd csle-ryu; pip install -e .; cd ..
+cd csle-common; pip install -e .; cd ..
+cd csle-cluster; pip install -e .; cd ..
+cd csle-attacker; pip install -e .; cd ..
+cd csle-defender; pip install -e .; cd ..
+cd gym-csle-intrusion-response-game; pip install -e .; cd ..
+cd csle-system-identification; pip install -e .; cd ..
+cd gym-csle-stopping-game; pip install -e .; cd ..
+cd csle-tolerance; pip install -e .; cd ..
+cd csle-agents; pip install -e .; cd ..
+cd csle-rest-api; pip install -e .; cd ..
+cd csle-cli; pip install -e .; cd ..
