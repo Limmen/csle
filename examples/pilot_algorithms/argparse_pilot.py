7182063841b8ad9b287c36470db2bdc860703516
nforsg <forsgren.nils@gmail.com>
2023-12-09 14:59:36 +0100

    work in progress

--- examples/pilot_algorithms/argparse_pilot.py
+++ examples/pilot_algorithms/argparse_pilot.py
@@ -1,11 +1,12 @@
-from argparse import ArgumentParser, Namespace
+from argparse import ArgumentParser
 
 parser = ArgumentParser()
 
 parser.add_argument('square', help ='calculates square', type=int)
-parser.add_argument('-v', '--verbose', help='verbose description', action='store true') 
+parser.add_argument('-v', '--verbose', help='verbose description', action='store_true') 
 args = parser.parse_args()
 
+print(args.verbose)
 if args.verbose:
     print(f'{args.square} square is: {args.square ** 2}')
 else:
