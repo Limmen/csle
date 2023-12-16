887b69897d3a2ad687f3c8c34384170f30e02a57
foroogh shahab <fo.shahab@gmail.com>
2023-12-16 20:13:23 +0100

    unit test csle-collector

--- simulation-system/libs/csle-common/tests/test_consumer_threads.py
+++ simulation-system/libs/csle-common/tests/test_consumer_threads.py
@@ -5,7 +5,21 @@ from csle_collector.host_manager.dao.host_metrics import HostMetrics
 from csle_common.consumer_threads.host_metrics_consumer_thread import HostMetricsConsumerThread
 from csle_common.consumer_threads.aggregated_host_metrics_thread import AggregatedHostMetricsThread
 from csle_collector.ossec_ids_manager.dao.ossec_ids_alert_counters import OSSECIdsAlertCounters
+from csle_collector.snort_ids_manager.dao.snort_ids_alert_counters import SnortIdsAlertCounters
+from csle_collector.snort_ids_manager.dao.snort_ids_rule_counters import SnortIdsRuleCounters
 from csle_common.consumer_threads.aggregated_ossec_ids_log_consumer_thread import AggregatedOSSECIdsLogConsumerThread
+from csle_common.consumer_threads.aggregated_snort_ids_log_consumer_thread import AggregatedSnortIdsLogConsumerThread
+from csle_common.consumer_threads.aggregated_snort_ids_rule_log_consumer_thread \
+    import AggregatedSnortIdsRuleLogConsumerThread
+from csle_common.dao.emulation_action.attacker.emulation_attacker_action import EmulationAttackerAction
+from csle_common.dao.emulation_action.attacker.emulation_attacker_action_type import EmulationAttackerActionType
+from csle_common.dao.emulation_action.attacker.emulation_attacker_action_id import EmulationAttackerActionId
+from csle_common.consumer_threads.attacker_actions_consumer_thread import AttackerActionsConsumerThread
+from csle_common.dao.emulation_observation.defender.emulation_defender_machine_observation_state import \
+    EmulationDefenderMachineObservationState
+from csle_collector.docker_stats_manager.dao.docker_stats import DockerStats
+from csle_collector.snort_ids_manager.dao.snort_ids_ip_alert_counters import SnortIdsIPAlertCounters
+from csle_common.consumer_threads.avg_host_metrics_thread import AvgHostMetricsThread
 
 
 class TestConsumerThreadsSuiteSuite:
@@ -175,3 +189,195 @@ class TestConsumerThreadsSuiteSuite:
             assert (aggregated_ossec_ids_alert_counters.group_alerts[idx] ==
                     example_ossec_ids_alert_counters.group_alerts[idx] +
                     example_ossec_ids_alert_counters_2.group_alerts[idx])
+
+    def test_aggregated_snort_ids_log_consumer_thread(self) -> None:
+        """
+        Tests creation of a snort ids log consumer thread and its methods
+
+        :param example_snort_ids_alert_counters: an object of SnortIdsAlertCounters
+        :return: None
+        """
+
+        example_snort_ids_alert_counters = SnortIdsAlertCounters()
+
+        example_snort_ids_alert_counters.total_alerts = 10
+        example_snort_ids_alert_counters.severe_alerts = 54
+        example_snort_ids_alert_counters.warning_alerts = 3
+        example_snort_ids_alert_counters.alerts_weighted_by_priority = 0.7
+        example_snort_ids_alert_counters.ip = "1.2.3.4"
+        example_snort_ids_alert_counters.ts = 1234
+
+        example_snort_ids_alert_counters.priority_alerts = list(np.zeros(4))
+        for idx in range(4):
+            example_snort_ids_alert_counters.priority_alerts[idx] = random.randint(0, 10)
+
+        example_snort_ids_alert_counters.class_alerts = list(
+            np.zeros(len(set(constants.SNORT_IDS_ROUTER.SNORT_ALERT_IDS_ID.values()))))
+        for idx in range(len(example_snort_ids_alert_counters.class_alerts)):
+            example_snort_ids_alert_counters.class_alerts[idx] = random.randint(0, 10)
+
+        thread = AggregatedSnortIdsLogConsumerThread(
+            kafka_server_ip="1.2.3.4", kafka_port=1234, snort_ids_alert_counters=example_snort_ids_alert_counters.copy()
+        )
+
+        assert thread.snort_ids_alert_counters == example_snort_ids_alert_counters
+        assert thread.kafka_port == 1234
+        assert thread.kafka_server_ip == "1.2.3.4"
+
+        # Test empty list
+        thread.snort_ids_alert_counters_list = []
+        aggregated_snort_ids_alert_counters = thread.get_aggregated_ids_alert_counters()
+        assert aggregated_snort_ids_alert_counters == example_snort_ids_alert_counters
+
+        # Test singleton list
+        thread.snort_ids_alert_counters_list = [example_snort_ids_alert_counters.copy()]
+        aggregated_snort_ids_alert_counters = thread.get_aggregated_ids_alert_counters()
+        assert aggregated_snort_ids_alert_counters == example_snort_ids_alert_counters
+
+        # Test list with 10 identical objects
+        thread.snort_ids_alert_counters_list = []
+        for i in range(10):
+            thread.snort_ids_alert_counters_list.append(example_snort_ids_alert_counters.copy())
+        aggregated_snort_ids_alert_counters = thread.get_aggregated_ids_alert_counters()
+        assert (aggregated_snort_ids_alert_counters.total_alerts ==
+                10 * example_snort_ids_alert_counters.total_alerts)
+        assert (aggregated_snort_ids_alert_counters.severe_alerts ==
+                10 * example_snort_ids_alert_counters.severe_alerts)
+        assert (aggregated_snort_ids_alert_counters.warning_alerts ==
+                10 * example_snort_ids_alert_counters.warning_alerts)
+        assert (round(aggregated_snort_ids_alert_counters.alerts_weighted_by_priority, 3) ==
+                round(10 * example_snort_ids_alert_counters.alerts_weighted_by_priority, 3))
+        for idx in range(len(aggregated_snort_ids_alert_counters.priority_alerts)):
+            assert (aggregated_snort_ids_alert_counters.priority_alerts[idx] ==
+                    10 * example_snort_ids_alert_counters.priority_alerts[idx])
+        for idx in range(len(aggregated_snort_ids_alert_counters.class_alerts)):
+            assert (aggregated_snort_ids_alert_counters.class_alerts[idx] ==
+                    10 * example_snort_ids_alert_counters.class_alerts[idx])
+
+        # Test list with two different objects
+        example_snort_ids_alert_counters_2 = SnortIdsAlertCounters()
+        example_snort_ids_alert_counters_2.priority_alerts = list(np.zeros(4))
+        for idx in range(4):
+            example_snort_ids_alert_counters_2.priority_alerts[idx] = random.randint(0, 10)
+
+        example_snort_ids_alert_counters_2.class_alerts = list(
+            np.zeros(len(set(constants.SNORT_IDS_ROUTER.SNORT_ALERT_IDS_ID.values()))))
+        for idx in range(len(example_snort_ids_alert_counters_2.class_alerts)):
+            example_snort_ids_alert_counters_2.class_alerts[idx] = random.randint(0, 10)
+
+        example_snort_ids_alert_counters_2.total_alerts = 15
+        example_snort_ids_alert_counters_2.severe_alerts = 1
+        example_snort_ids_alert_counters_2.warning_alerts = 18
+        example_snort_ids_alert_counters_2.alerts_weighted_by_priority = 0.7
+
+        thread.snort_ids_alert_counters_list = [example_snort_ids_alert_counters,
+                                                example_snort_ids_alert_counters_2]
+        aggregated_snort_ids_alert_counters = thread.get_aggregated_ids_alert_counters()
+
+        assert (aggregated_snort_ids_alert_counters.severe_alerts ==
+                example_snort_ids_alert_counters.severe_alerts + example_snort_ids_alert_counters_2.severe_alerts)
+        assert (aggregated_snort_ids_alert_counters.warning_alerts ==
+                example_snort_ids_alert_counters.warning_alerts + example_snort_ids_alert_counters_2.warning_alerts)
+        assert (aggregated_snort_ids_alert_counters.total_alerts ==
+                example_snort_ids_alert_counters.total_alerts + example_snort_ids_alert_counters_2.total_alerts)
+        assert (round(aggregated_snort_ids_alert_counters.alerts_weighted_by_priority, 3) ==
+                round(example_snort_ids_alert_counters.alerts_weighted_by_priority +
+                      example_snort_ids_alert_counters_2.alerts_weighted_by_priority, 3))
+        for idx in range(len(aggregated_snort_ids_alert_counters.priority_alerts)):
+            assert (aggregated_snort_ids_alert_counters.priority_alerts[idx] ==
+                    example_snort_ids_alert_counters.priority_alerts[idx] +
+                    example_snort_ids_alert_counters_2.priority_alerts[idx])
+        for idx in range(len(aggregated_snort_ids_alert_counters.class_alerts)):
+            assert (aggregated_snort_ids_alert_counters.class_alerts[idx] ==
+                    example_snort_ids_alert_counters.class_alerts[idx] +
+                    example_snort_ids_alert_counters_2.class_alerts[idx])
+
+    def test_aggregated_snort_ids_rule_log_consumer_thread(self) -> None:
+        """
+        Tests creation of a snort ids rule log consumer thread and its methods
+
+        :param example_snort_ids_rule_alert_counters: an object of SnortIdsRuleAlertCounters
+        :return: None
+        """
+        example_snort_ids_rule_alert_counters = SnortIdsRuleCounters()
+
+        example_snort_ids_rule_alert_counters.rule_alerts = {}
+        example_snort_ids_rule_alert_counters.rule_alerts[1] = 10
+        example_snort_ids_rule_alert_counters.rule_alerts[2] = 3
+        example_snort_ids_rule_alert_counters.ip = "1.2.3.4"
+        example_snort_ids_rule_alert_counters.ts = 1234
+
+        thread = AggregatedSnortIdsRuleLogConsumerThread(
+            kafka_server_ip="1.2.3.4", kafka_port=1234,
+            snort_ids_rule_counters=example_snort_ids_rule_alert_counters.copy()
+        )
+
+        assert thread.snort_ids_rule_counters == example_snort_ids_rule_alert_counters
+        assert thread.kafka_port == 1234
+        assert thread.kafka_server_ip == "1.2.3.4"
+
+        # Test empty list
+        thread.snort_ids_rule_counters_list = []
+        aggregated_snort_ids_rule_alert_counters = thread.get_aggregated_ids_rule_counters()
+        assert aggregated_snort_ids_rule_alert_counters == example_snort_ids_rule_alert_counters
+
+        # Test singleton list
+        thread.snort_ids_rule_counters_list = [example_snort_ids_rule_alert_counters.copy()]
+        aggregated_snort_ids_rule_alert_counters = thread.get_aggregated_ids_rule_counters()
+        assert aggregated_snort_ids_rule_alert_counters == example_snort_ids_rule_alert_counters
+
+        # Test list with 10 identical objects
+        thread.snort_ids_rule_counters_list = [example_snort_ids_rule_alert_counters.copy()] * 10
+        aggregated_snort_ids_rule_alert_counters = thread.get_aggregated_ids_rule_counters()
+
+        for k, v in aggregated_snort_ids_rule_alert_counters.rule_alerts.items():
+            assert (aggregated_snort_ids_rule_alert_counters.rule_alerts[k] ==
+                    10 * example_snort_ids_rule_alert_counters.rule_alerts[k])
+
+        # Test list with two different objects
+        example_snort_ids_rule_alert_counters_2 = SnortIdsRuleCounters()
+        example_snort_ids_rule_alert_counters_2.rule_alerts = {}
+        example_snort_ids_rule_alert_counters_2.rule_alerts[1] = 12
+        example_snort_ids_rule_alert_counters_2.rule_alerts[2] = 8
+        example_snort_ids_rule_alert_counters_2.ip = "1.2.3.4"
+        example_snort_ids_rule_alert_counters_2.ts = 1234
+
+        thread.snort_ids_rule_counters_list = [example_snort_ids_rule_alert_counters,
+                                               example_snort_ids_rule_alert_counters_2]
+        aggregated_snort_ids_rule_alert_counters = thread.get_aggregated_ids_rule_counters()
+
+        for k, v in aggregated_snort_ids_rule_alert_counters.rule_alerts.items():
+            assert (aggregated_snort_ids_rule_alert_counters.rule_alerts[k] ==
+                    example_snort_ids_rule_alert_counters.rule_alerts[k] +
+                    example_snort_ids_rule_alert_counters_2.rule_alerts[k])
+
+    def test_attacker_actions_consumer_thread(self) -> None:
+        """
+        Tests creation of a attacker action consumer thread and its methods
+
+        :return: None
+        """
+        example_attacker_actions_consumer = EmulationAttackerAction(
+            id=EmulationAttackerActionId.HTTP_ENUM_HOST, name="test1", cmds=["test1"],
+            type=EmulationAttackerActionType.CONTINUE, descr="test", ips=["1.1.2.3"], index=1)
+        thread = AttackerActionsConsumerThread(kafka_server_ip="1.1.1.1", kafka_port=1234,
+                                               attacker_actions=[example_attacker_actions_consumer])
+        assert thread.attacker_actions[0] == example_attacker_actions_consumer
+
+    def test_avg_host_metrics_thread(self) -> None:
+        """
+        Tests creation of a avg host metrics thread and its methods
+
+        :return: None
+        """
+        example_host_metrics = HostMetrics(num_logged_in_users=1, num_failed_login_attempts=10,
+                                           num_open_connections=1, num_login_events=2,
+                                           num_processes=5, num_users=1, ip="1.1.1.1", ts=0.5)
+        emulation_defender_machine_observation_state_example = EmulationDefenderMachineObservationState(
+            ips=["test"], kafka_config=None, host_metrics=example_host_metrics,
+            docker_stats=DockerStats(), snort_ids_ip_alert_counters=SnortIdsIPAlertCounters(),
+            ossec_ids_alert_counters=OSSECIdsAlertCounters())
+        thread = AvgHostMetricsThread(host_metrics=example_host_metrics,
+                                      machines=[emulation_defender_machine_observation_state_example], sleep_time=1)
+        assert thread.host_metrics == example_host_metrics
+        assert thread.machines[0] == emulation_defender_machine_observation_state_example
