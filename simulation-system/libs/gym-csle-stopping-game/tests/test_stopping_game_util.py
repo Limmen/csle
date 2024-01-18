fe5275b518945f299b1d66f7d028b573f7e2a628
foroogh shahab <fo.shahab@gmail.com>
2024-01-18 22:33:01 +0100

    unit test gym-csle-stopping-game

--- simulation-system/libs/gym-csle-stopping-game/tests/test_stopping_game_util.py
+++ simulation-system/libs/gym-csle-stopping-game/tests/test_stopping_game_util.py
@@ -59,3 +59,49 @@ class TestStoppingGameUtilSuite(object):
         l = 6
         example_reward_vector = StoppingGameUtil.reward_tensor(R_SLA=1, R_INT=3, R_COST=2, L=l, R_ST=5)
         assert example_reward_vector.shape == (l, 2, 2, 3)
+
+    def test_transition_tensor(self) -> None:
+        """
+        Tests the transition tensor function
+
+        :return: None
+        """
+        l = 6
+        example_transition_tensor = StoppingGameUtil.transition_tensor(L=l, p=0.1)
+        for i in range(l):
+            for j in range(2):
+                for k in range(2):
+                    for m in range(3):
+                        assert sum(example_transition_tensor[i, j, k, m])
+        assert example_transition_tensor.shape == (l, 2, 2, 3, 3)
+
+    def test_observation_tensor(self) -> None:
+        """
+        Tests the observation tensor function
+
+        :return: None
+        """
+        n = 6
+        example_observation_tensor = StoppingGameUtil.observation_tensor(n=n)
+        assert example_observation_tensor.shape == (2, 2, 3, n + 1)
+
+    def test_sample_next_state(self, example_stopping_game_util: StoppingGameUtil) -> None:
+        """
+        Tests the sample next state function
+
+        :return: None
+        """
+        example_sample_next_state = StoppingGameUtil.sample_next_state(
+            T=example_stopping_game_util.transition_tensor(L=3, p=0.1), l=3, s=2, a1=1, a2=1,
+            S=example_stopping_game_util.state_space())
+        assert example_sample_next_state in [0, 1, 2]
+
+    def test_sample_initial_state(self, example_stopping_game_util: StoppingGameUtil) -> None:
+        """
+        Tests the sample initial state function
+
+        :return: None
+        """
+        example_sample_initial_state = StoppingGameUtil.sample_initial_state(example_stopping_game_util.b1())
+        assert example_sample_initial_state in example_stopping_game_util.b1()
+        assert sum(example_stopping_game_util.b1()) == 1
