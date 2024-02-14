3917846593dda26ef1e5195eb3a565b0c767ce29
Kim Hammar <kimham@kth.se>
2024-02-14 14:35:19 +0100

    tests

--- examples/eval/cyborg_scenario_two/pomcp_eval_3.py
+++ examples/eval/cyborg_scenario_two/pomcp_eval_3.py
@@ -63,7 +63,7 @@ if __name__ == '__main__':
     num_evaluations = 100
     max_horizon = 100
     returns = []
-    seed = 290078
+    seed = 2228719
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
