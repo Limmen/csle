f4095dc4209a4b1da7cc08bd47073e78248a57de
Limmen <kimham@kth.se>
2024-03-10 22:17:50 +0100

    ansible installation [wip]

--- metastore/create_db.sql
+++ metastore/create_db.sql
@@ -2,10 +2,10 @@
 SELECT 'CREATE DATABASE csle' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'csle')\gexec
 
 -- Create csle user --
-REASSIGN OWNED BY csle TO postgres;
-DROP OWNED BY csle;
 DROP USER IF EXISTS csle;
 CREATE USER csle WITH ENCRYPTED PASSWORD 'csle';
+REASSIGN OWNED BY csle TO postgres;
+DROP OWNED BY csle;
 
 -- Grant priviliges to csle user for the csle db --
 GRANT ALL PRIVILEGES ON DATABASE csle TO csle;
