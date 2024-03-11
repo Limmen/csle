79cb15b01fdfc00dd3b4d3a2fc2a5215ba6602af
Limmen <kimham@kth.se>
2024-03-11 14:59:50 +0100

    ansible installation [wip]

---
+++ ansible/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+csle stop clustermanager
+sleep 2
+csle start clustermanager
+sleep 2
+csle start cadvisor
+csle start grafana
+csle start prometheus
+csle start node_exporter
+csle start flask
+csle start nginx
