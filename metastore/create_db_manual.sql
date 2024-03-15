bef409bd9f899450a5d7756fff09c4b2d93722e8
Limmen <kimham@kth.se>
2024-03-15 13:18:54 +0100

    update version

---
+++ metastore/create_db_manual.sql
@@ -0,0 +1,18 @@
+--  Create csle db if not exists --
+SELECT 'CREATE DATABASE csle'
+    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'csle')\gexec
+
+-- Connect to the csle db --
+\connect csle
+
+-- Create csle user --
+REASSIGN OWNED BY csle TO postgres;
+DROP OWNED BY csle;
+DROP USER IF EXISTS csle;
+CREATE USER csle WITH ENCRYPTED PASSWORD 'csle';
+
+-- Grant priviliges to csle user for the csle db --
+GRANT ALL PRIVILEGES ON DATABASE csle TO csle;
+
+-- Create CITUS extension --
+CREATE EXTENSION citus;⏎
