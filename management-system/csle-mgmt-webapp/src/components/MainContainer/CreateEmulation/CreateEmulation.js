d03b218005fa768f1ace1af408a10a8290a56878
foroogh shahab <fo.shahab@gmail.com>
2024-03-29 19:05:55 +0100

    the page is setup by Kim

---
+++ management-system/csle-mgmt-webapp/src/components/MainContainer/CreateEmulation/CreateEmulation.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+import Select from 'react-select'
+import './CreateEmulation.css';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+
+/**
+ * Component representing the /create-emulation-page
+ */
+const CreateEmulation = (props) => {
+
+
+    return (
+        <div className="CreateEmulation">
+            <h3 className="managementTitle"> Create Emulation </h3>
+        </div>
+    );
+}
+
+CreateEmulation.propTypes = {};
+CreateEmulation.defaultProps = {};
+export default CreateEmulation;
