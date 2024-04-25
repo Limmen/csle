6696522767dd9fa04e93578611c9caf1fdfa1576
Bength Pappila <brpa@kth.se>
2024-04-25 13:22:46 +0200

    Refactor Kullback function
    Refactored Kullback function and added tests.
    Updated folder structure and moved files.

---
+++ simulation-system/libs/csle-attack-profiler/tests/test_kullback.py
@@ -0,0 +1,25 @@
+
+from csle_attack_profiler.dao.kullback_for_all import KullbackLeibler
+from csle_common.dao.system_identification.emulation_statistics import EmulationStatistics
+import os
+
+class TestKullbackSuite:
+
+    def test_kullback_counts(self) -> None:
+
+        p = [1, 4, 1, 4]
+        q = [5, 3, 5, 1]
+
+        kl_div = KullbackLeibler.kullback_leibler_divergence_for_counts(p, q)
+
+        assert kl_div == 0.6841752275630617, f"Test failed! Expected: 0.03669001465441545, Actual: {kl_div}"
+
+    def test_kullback_statistics(self) -> None:
+        
+        current_dir = os.path.dirname(__file__)
+        path = os.path.join(current_dir, "statistics_test.json")
+        statistics = EmulationStatistics.from_json_file(path)
+
+        kl_div, kl_div2 = KullbackLeibler.kullback_leibler_for_metric("alerts_weighted_by_priority", "A:MyTest", statistics)
+
+        assert kl_div and kl_div2, f"Test failed! Expect values, Actual: {kl_div}, {kl_div2}"
