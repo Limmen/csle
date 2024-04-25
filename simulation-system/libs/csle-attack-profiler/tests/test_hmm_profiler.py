fcfa1248adbe0cb549644b0b4d77dd43a75f1985
Bength Pappila <brpa@kth.se>
2024-04-25 14:54:39 +0200

    Refactor HMMProfiling,
    Cleanup HMMProfiling, and add additional tests based on test data.

--- simulation-system/libs/csle-attack-profiler/tests/test_hmm_profiler.py
+++ simulation-system/libs/csle-attack-profiler/tests/test_hmm_profiler.py
@@ -1,11 +1,16 @@
 from csle_common.dao.system_identification.emulation_statistics import EmulationStatistics
+from csle_common.dao.emulation_action.attacker.emulation_attacker_action_id \
+    import EmulationAttackerActionId
 from csle_attack_profiler.hmm_profiling import HMMProfiler 
 import numpy as np
+import os
 
 class TestHMMProfilerSuite:
+    """
+    Test suite for the HMMProfiler class.
+    """
 
-
-    def test_viterbi(self):
+    def test_viterbi(self) -> None:
         """
         Test the viterbi algorithm.
         Source: https://www.cis.upenn.edu/~cis2620/notes/Example-Viterbi-DNA.pdf
@@ -26,7 +31,6 @@ class TestHMMProfilerSuite:
         # Consider the sequence GGCACTGAA
         seq = [2, 2, 1, 0, 1, 3, 2, 0, 0]
 
-        # Define the list of possible emissions
 
         # Run the Viterbi algorithm
         path = HMMProfiler.viterbi(hidden_states, init_probs, trans_matrix, emission_matrix, seq, emissions_list)
@@ -35,54 +39,28 @@ class TestHMMProfilerSuite:
         expected_path = [0, 0, 0, 1, 1, 1, 1, 1, 1]
         assert np.array_equal(path, expected_path), f"Test failed! Expected: {expected_path}, Actual: {path}"
 
-    def test_hmm_profiler(self):
+    def test_hmm_profiler_test_data(self) -> None:
         """
-        Test the HMMProfiler class.
+        Test the HMMProfiler class with test statistics.
         """
-
-        STATES = ['no_intrusion', 'A:TCP SYN (Stealth) Scan', 'A:SSH dictionary attack for username=pw', 
-                                  'A:Telnet dictionary attack for username=pw', 'A:FTP dictionary attack for username=pw', 
-                                  'A:Network service login','A:Install tools', 'ssh backdoor', 'A:ShellShock Explolit', 'A:CVE-2010-0426 exploit', 
-                                  'A:Ping Scan', 'A:Sambacry Explolit', 'A:DVWA SQL Injection Exploit', 'A:CVE-2015-1427 exploit']
+        STATES = ['no_intrusion', 'A:CVE-2015-1427 exploit']
     
-        TRANSITION_MATRIX = [[1/10, 3/10, 0, 0, 0, 0, 0, 0, 0, 0, 6/10, 0, 0, 0],
-                         [0, 0, 0.5, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0],
-                         [0, 0, 0, 1/3, 0, 2/3, 0, 0, 0, 0, 0, 0, 0, 0],
-                         [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                         [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-                         [0, 0, 0, 0, 0, 0, 9/11, 0, 0, 2/11, 0, 0, 0, 0],
-                         [0, 0, 1/9, 0, 0, 0, 0, 1/9, 0, 0, 7/9, 0, 0, 0],
-                         [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                         [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-                         [0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0],
-                         [0, 0, 1/8, 0, 0, 0, 0, 0, 0, 0, 0, 3/8, 1/4, 1/4],
-                         [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-                         [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-                         [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]]
-    
-        METRICS = ['alerts_weighted_by_priority', 'cpu_percent', 'default-login-attempt_alerts', 'mem_current',
-                    'net_rx', 'net_tx', 'num_clients', 'num_failed_login_attempts', 'num_open_connections', 'num_processes',
-                    'pids', 'priority_3_alerts', 'priority_4_alerts', 'successful-user_alerts', 'total_alerts',
-                    'warning_alerts']
+        TRANSITION_MATRIX = [[1/3,2/3],[1/2,1/2]]
 
-        statistics = EmulationStatistics.from_json_file("./statistics.json")
-        statistics = EmulationStatistics.from_json_file("./statistics2.json")
+        METRIC = 'alerts_weighted_by_priority'
+
+        current_dir = os.path.dirname(__file__)
+        path = os.path.join(current_dir, "statistics_test.json")
+        statistics = EmulationStatistics.from_json_file(path)
+
         model = HMMProfiler([statistics])
 
-        model.create_model(TRANSITION_MATRIX, STATES, METRICS[0]) 
-        state_seq, observation_seq = model.generate_sequence(10, 0, 42)
+
+        model.create_model(TRANSITION_MATRIX, STATES, METRIC)
+        state_seq, observation_seq = model.generate_sequence(2, 0, 42)
         path = model.profile_sequence(observation_seq)
-        converted_state_seq = model.convert_states_to_profiles(state_seq)
-        
-        converted_path = model.convert_states_to_profiles(path) # Delete before commit
-        
-        for i in range(len(path)):
-            assert path[i] == state_seq[i], f"Test failed! Expected: {state_seq}, Actual: {path}"
-        assert path == state_seq, "Test failed! The path is None."
-        assert converted_path == converted_state_seq, f"Test failed! Expected: {converted_state_seq}, Actual: {converted_path}" # Delete before commit
+        converted_path = model.convert_states_to_profiles(path[:1])
 
-
-        # Test the get_transition_matrix method
-
-
-
+        assert path[0] == 'A:CVE-2015-1427 exploit', "Test failed! The first profiled action in path is incorrect."
+        assert state_seq[0] == 'no_intrusion', "Test failed! The first profiled action in simulated state sequence is incorrect."
+        assert converted_path[0].action_id == EmulationAttackerActionId.CVE_2015_1427_EXPLOIT, "Test failed! The converted path fails."
