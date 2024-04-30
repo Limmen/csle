f35b6f4ce7df536ba208ed2b056c0171794a13ee
Bength Pappila <brpa@kth.se>
2024-04-30 09:56:11 +0200

    fix flake8 for attack_graph

--- simulation-system/libs/csle-attack-profiler/src/csle_attack_profiler/dao/attack_graph.py
+++ simulation-system/libs/csle-attack-profiler/src/csle_attack_profiler/dao/attack_graph.py
@@ -1,25 +1,26 @@
 from csle_attack_profiler.dao.tactics import Tactics
-from typing import List, Tuple, Union, Optional
-
+from typing import List, Tuple, Union
 
 ChildNode = Tuple[Tactics, int]
 Node = Tuple[Tactics, List[ChildNode], int]
+
 
 class AttackGraph:
     """
     Class representing the attack graph
     """
 
-
     def __init__(self) -> None:
         """
         Class contructor
 
-        The graph is represented as a list of tuples. Each tuple contains the node name, the children of the node and the node id.
+        The graph is represented as a list of tuples. Each tuple contains the node name,
+        the children of the node and the node id.
         """
         self.graph: List[Node] = []
 
-    def add_node(self, node_name: Tactics, children: Union[List[ChildNode], None] = None, node_id: Union[int, None] = None) -> None:
+    def add_node(self, node_name: Tactics, children: Union[List[ChildNode], None] = None,
+                 node_id: Union[int, None] = None) -> None:
         """
         Add a node to the graph
 
@@ -35,7 +36,8 @@ class AttackGraph:
             children = []
         self.graph.append((node_name, children, node_id))
 
-    def add_edge(self, parent_node_name: Tactics, parent_node_id: int, child_node_name: Tactics, child_node_id: int) -> None:
+    def add_edge(self, parent_node_name: Tactics, parent_node_id: int,
+                 child_node_name: Tactics, child_node_id: int) -> None:
         """
         Add an edge to the graph by defining the parent node and the children
 
@@ -45,13 +47,12 @@ class AttackGraph:
         for i, (node_name, children, node_id) in enumerate(self.graph):
             if node_name == parent_node_name and node_id == parent_node_id:
 
-                if any(child[0] == child_node_name for child in children):    
+                if any(child[0] == child_node_name for child in children):
                     raise RuntimeError("Child node already exists in the parent node")
                 else:
                     self.graph[i][1].append((child_node_name, child_node_id))
 
                 break
-
 
     def get_node(self, node_name: Tactics, node_id: int) -> Union[None, Node]:
         """
@@ -74,7 +75,6 @@ class AttackGraph:
         """
         return self.graph[0]
     
-            
     def get_children(self, node_name: Tactics, node_id: int) -> Union[None, List[ChildNode]]:
         """
         Get the children of the node
@@ -88,5 +88,3 @@ class AttackGraph:
             if node_name == node[0] and node[2] == node_id:
                 return node[1]
         return None
-
-
