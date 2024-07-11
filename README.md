3ae714f65e08bcb0c85766212ebccbe675982ef5
Kim Hammar <kimham@kth.se>
2024-07-11 17:12:24 +0200

    update contributors

--- README.md
+++ README.md
@@ -168,6 +168,7 @@ Thanks go to these people!
     </tr>
     <tr>
        <td align="center" valign="top" width="14.28%"><a href="https://github.com/kingxiaofire"><img src="https://github.com/kingxiaofire.png" width="100px;" alt="Yan Wang"/><br /><sub><b>Yan Wang</b></sub></a></td>
+       <td align="center" valign="top" width="14.28%"><a href="https://github.com/Awsnaser"><img src="https://github.com/Awsnaser.png" width="100px;" alt="Aws Jaber"/><br /><sub><b>Aws Jaber</b></sub></a></td>
     </tr>
   </tbody>
 </table>
--- examples/eval/cyborg_scenario_two/evaluate_on_wrapper_env.py
+++ examples/eval/cyborg_scenario_two/evaluate_on_wrapper_env.py
@@ -3,12 +3,14 @@ import torch
 import random
 from csle_common.metastore.metastore_facade import MetastoreFacade
 from gym_csle_cyborg.envs.cyborg_scenario_two_wrapper import CyborgScenarioTwoWrapper
+from gym_csle_cyborg.dao.red_agent_type import RedAgentType
 from gym_csle_cyborg.dao.csle_cyborg_wrapper_config import CSLECyborgWrapperConfig
 
 if __name__ == '__main__':
     ppo_policy = MetastoreFacade.get_ppo_policy(id=58)
     config = CSLECyborgWrapperConfig(maximum_steps=100, gym_env_name="",
-                                     save_trace=False, reward_shaping=False, scenario=2)
+                                     save_trace=False, reward_shaping=False, scenario=2,
+                                     red_agent_type=RedAgentType.B_LINE_AGENT)
     env = CyborgScenarioTwoWrapper(config=config)
     num_evaluations = 10000
     max_horizon = 100
@@ -25,6 +27,7 @@ if __name__ == '__main__':
         while t < max_horizon:
             a = ppo_policy.action(o=o)
             o, r, done, _, info = env.step(a)
+            env.show
             R += r
             t += 1
         returns.append(R)
