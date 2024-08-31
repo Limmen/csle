41e93518073a6add8a33a20f57f32001d92e835e
Limmen <kimham@kth.se>
2024-08-31 15:55:57 +0200

    release python 0.7.0

--- simulation-system/libs/gym-csle-stopping-game/docs/source/conf.py
+++ simulation-system/libs/gym-csle-stopping-game/docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020-2024, Kim Hammar'
 author = 'Kim Hammar'
 
 # The full version, including alpha/beta/rc tags
-release = '0.6.0'
+release = '0.7.0'
 
 
 # -- General configuration ---------------------------------------------------
