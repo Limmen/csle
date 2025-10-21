a62808fa39aad44b7035135301c4600af15a966f
Kim Hammar <kimham@kth.se>
2025-10-21 12:03:19 +0800

    update estlint config file

---
+++ management-system/csle-mgmt-webapp/eslintrc.mjs
@@ -0,0 +1,39 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import react from "eslint-plugin-react";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([globalIgnores(["build/*.js", "build/*", "'.build/*'"]), {
+    extends: compat.extends(
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "standard",
+        "plugin:prettier/recommended",
+    ),
+
+    plugins: {
+        react,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+        },
+
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+
+    rules: {},
+}]);
